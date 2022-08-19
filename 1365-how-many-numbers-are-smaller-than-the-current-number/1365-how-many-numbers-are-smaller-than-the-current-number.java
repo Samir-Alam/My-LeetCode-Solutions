589658c8d2d31617276f03ca0503043805884913
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-08-20 01:33:50 +0530

    Time: 1 ms (99.99%), Space: 45.1 MB (15.59%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -15,21 +15,36 @@ class Solution {
         // }
         // return ans;
         
+        
         //--------2nd Solution--------
         
-        int temp[] = Arrays.copyOf(nums, n);
-        Arrays.sort(temp);
-        // Map<Integer, Integer> map = HashMap<>();
-        for(int i = 0; i < n; i++){
-            if(i == 0)
-                ans[temp[i]] = 0;
-            else if(temp[i] != temp[i - 1])
-                ans[temp[i]] = i;
-            else
-                ans[temp[i]] = ans[temp[i - 1]]; 
-        }
-        for(int i = 0; i < n; i++)
-            temp[i] = ans[nums[i]];
-        return temp;
+        // int temp[] = Arrays.copyOf(nums, n);
+        // Arrays.sort(temp);
+        // for(int i = 0; i < n; i++){
+        //     if(i == 0)
+        //         ans[temp[i]] = 0;
+        //     else if(temp[i] != temp[i - 1])
+        //         ans[temp[i]] = i;
+        //     else
+        //         ans[temp[i]] = ans[temp[i - 1]]; 
+        // }
+        // for(int i = 0; i < n; i++)
+        //     temp[i] = ans[nums[i]];
+        // return temp;
+        
+        
+        //--------3rd Solution--------
+        
+        int[] bucket = new int[102];
+        for(int i = 0; i < nums.length; i++)
+            bucket[nums[i] + 1]++;
+
+        for(int i = 1; i < 102; i++) 
+            bucket[i] += bucket[i - 1];
+        
+        for(int i = 0; i<nums.length; i++)
+            nums[i] = bucket[nums[i]];
+        
+        return nums;
     }
 }
