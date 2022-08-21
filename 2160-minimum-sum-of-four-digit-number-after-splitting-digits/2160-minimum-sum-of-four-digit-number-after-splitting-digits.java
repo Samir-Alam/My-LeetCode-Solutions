b840c7705889f89b560aabd30128da0da4076820
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-08-21 21:02:57 +0530

    Time: 1 ms (86.52%), Space: 40.7 MB (71.23%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -7,17 +7,17 @@ class Solution {
             num /= 10;
         }
         Collections.sort(arr);
-        arr.removeAll(Collections.singleton(0));
-        int n = 0;
+        // arr.removeAll(Collections.singleton(0));
+//         int n = 0;
         
-        if(arr.size() == 1)
-            n = arr.get(0);
-        else if(arr.size() == 2)
-            n = arr.get(0) + arr.get(1);
-        else if(arr.size() == 3)
-            n = arr.get(0)*10 + arr.get(1) + arr.get(2);
-        else if(arr.size() == 4)
-            n = arr.get(0)*10 + arr.get(3) + arr.get(1)*10 + arr.get(2);
-        return n;
+//         if(arr.size() == 1)
+//             n = arr.get(0);
+//         else if(arr.size() == 2)
+//             n = arr.get(0) + arr.get(1);
+//         else if(arr.size() == 3)
+//             n = arr.get(0)*10 + arr.get(1) + arr.get(2);
+//         else if(arr.size() == 4)
+//             n = arr.get(0)*10 + arr.get(3) + arr.get(1)*10 + arr.get(2);
+        return arr.get(0)*10 + arr.get(3) + arr.get(1)*10 + arr.get(2);
     }
 }
