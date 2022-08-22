2aac1fc9a69635ff36b8feadd3f6abb05dec243b
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-08-22 19:19:09 +0530

    Time: 4 ms (11.07%), Space: 41.3 MB (50.03%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n == 1)  return true;
+        if(((n%2 == 1) || (n == 0)) && n != 1)
+            return false;
+        // int ans = 4;
+        // for(int i = 1; i < n; i++){
+        //     ans = (int)Math.pow(4, i);
+        //     if(ans == n)
+        //         return true;
+        //     else if(ans > n)
+        //         break;
+        // }
+        // return ans == n;
+        return CountTrailingZeros(n);
+    }
+    static boolean CountTrailingZeros(int n)
+    {
+         
+        String bit = Integer.toBinaryString(n);
+        StringBuilder bit1 = new StringBuilder();
+        bit1.append(bit);
+        int zero = 0;
+      
+        for (int i = 1; i < 64; i++) {
+            if (i < bit1.length() && bit1.charAt(i) == '0')
+                zero++;
+            else if(i < bit1.length() && bit1.charAt(i) == '1')
+                return false;
+        }
+      
+        return zero%2 == 0;
+    }
+}
