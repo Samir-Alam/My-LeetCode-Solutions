d5de7625abfa13b2fc0ddd0e4bdad7b5c0089d74
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-08-26 08:03:49 +0530

    Time: 1 ms (100.00%), Space: 41.6 MB (26.67%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-        char[] res1 = String.valueOf(n).toCharArray();
-        Arrays.sort(res1);
-        for (int i = 0; i < 30; i++) {
-            char[] res2 = String.valueOf(1 << i).toCharArray();
-            Arrays.sort(res2);
-            if (Arrays.equals(res1, res2)) return true;
-        }
+        // char[] res1 = String.valueOf(n).toCharArray();
+        // Arrays.sort(res1);
+        // for (int i = 0; i < 30; i++) {
+        //     char[] res2 = String.valueOf(1 << i).toCharArray();
+        //     Arrays.sort(res2);
+        //     if (Arrays.equals(res1, res2)) return true;
+        // }
+        // return false;
+        
+        long c = counter(n);
+        for (int i = 0; i < 32; i++)
+            if (counter(1 << i) == c) return true;
         return false;
+    }
+    public long counter(int N) {
+        long res = 0;
+        for (; N > 0; N /= 10) res += (int)Math.pow(10, N % 10);
+        return res;
     }
 }
