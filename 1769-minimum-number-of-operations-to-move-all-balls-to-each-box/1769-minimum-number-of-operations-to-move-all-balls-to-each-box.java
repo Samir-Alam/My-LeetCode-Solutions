109aa104b993365e6d685297511cdbb78f50fcf4
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-08-27 22:55:47 +0530

    Time: 6 ms (69.92%), Space: 47.1 MB (77.86%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -14,19 +14,19 @@ class Solution {
         //2nd Solution
         int len = boxes.length();
         int[] forward = new int[len];
-        int prev = (boxes.charAt(0)=='0'?0:1);
-        for(int i=1;i<len;i++){
-            forward[i] += forward[i-1]+prev;
-            prev += (boxes.charAt(i)=='0'?0:1);
+        int prev = (boxes.charAt(0) == '0' ? 0 : 1);
+        for(int i = 1; i < len; i++){
+            forward[i] += forward[i - 1] + prev;
+            prev += (boxes.charAt(i)=='0' ? 0 : 1);
         }
-        prev = (boxes.charAt(len-1)=='0'?0:1);
+        prev = (boxes.charAt(len-1)=='0' ? 0 : 1);
         int[] back = new int[len];
-        for(int i=len-2;i>=0;i--){
-            back[i] += back[i+1]+prev;
-            prev += (boxes.charAt(i)=='0'?0:1);
+        for(int i = len - 2; i >= 0; i--){
+            back[i] += back[i + 1] + prev;
+            prev += (boxes.charAt(i) == '0'?0:1);
         }
         int[] op = new int[len];
-        for(int i=0;i<len;i++) op[i] = forward[i]+back[i];
+        for(int i = 0; i < len; i++) op[i] = forward[i] + back[i];
         return op;
     }
 }
