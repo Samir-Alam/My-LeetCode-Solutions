027be678e925dba5b5854ef0c93468861e6e8777
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-08-27 18:16:43 +0530

    Time: 1 ms (100.00%), Space: 45.1 MB (33.61%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -3,9 +3,6 @@ class Solution {
         char a[] = new char[s.length()];
         for(int i = 0; i < indices.length; i++)
             a[indices[i]] = s.charAt(i);
-        String ans = "";
-        for(char c : a)
-            ans += Character.toString(c);
-        return ans;
+        return String.valueOf(a);
     }
 }
