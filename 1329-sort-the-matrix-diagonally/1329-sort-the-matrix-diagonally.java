843a47a10b9fca3a3868269faa463ef86f081036
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-08-28 22:47:59 +0530

    Time: 10 ms (61.20%), Space: 47.8 MB (68.40%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,16 +1,27 @@
 class Solution {
-    public int[][] diagonalSort(int[][] A) {
-        int m = A.length, n = A[0].length;
-        HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                d.putIfAbsent(i - j, new PriorityQueue<>());
-                d.get(i - j).add(A[i][j]);
-            }
-        }
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j)
-                A[i][j] = d.get(i - j).poll();
-        return A;
+    public int[][] diagonalSort(int[][] mat) {
+        // int m = A.length, n = A[0].length;
+        // HashMap<Integer, PriorityQueue<Integer>> d = new HashMap<>();
+        // for (int i = 0; i < m; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         d.putIfAbsent(i - j, new PriorityQueue<>());
+        //         d.get(i - j).add(A[i][j]);
+        //     }
+        // }
+        // for (int i = 0; i < m; ++i)
+        //     for (int j = 0; j < n; ++j)
+        //         A[i][j] = d.get(i - j).poll();
+        // return A;
+        
+        int m = mat.length, n = mat[0].length;
+        for (int r = m - 1, c = 0; r >= 0; r--) fillMatrix(mat, m, n, r, c);
+        for (int r = 0, c = 1; c < n - 1; c++) fillMatrix(mat, m, n, r, c);
+        return mat;
+    }
+    private void fillMatrix(int[][] mat, int m, int n, int r, int c) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; r + i < m && c + i < n; i++) arr.add(mat[r + i][c + i]);
+        Collections.sort(arr);
+        for (int i = 0; r + i < m && c + i < n; i++) mat[r + i][c + i] = arr.get(i);
     }
 }
