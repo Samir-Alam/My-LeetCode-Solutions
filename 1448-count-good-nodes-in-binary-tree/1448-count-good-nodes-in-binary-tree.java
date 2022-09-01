ba068a2be06938b9b1066e0467e1be5f2c80dc04
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-01 20:35:29 +0530

    Time: 3 ms (76.91%), Space: 59.3 MB (74.49%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -23,18 +23,26 @@ class Node {
 }
 class Solution {
     public int goodNodes(TreeNode root) {
-        Deque<Node> stk = new ArrayDeque<>();
-        stk.push(new Node(root, root.val));
-        int cnt = 0;
-        while (!stk.isEmpty()) {
-            Node cur = stk.pop();
-            cnt += cur.tn.val >= cur.currentMax ? 1 : 0;
-            for (TreeNode kid : new TreeNode[]{cur.tn.left, cur.tn.right}) {
-                if (kid != null) {
-                    stk.push(new Node(kid, cur.currentMax));
-                }
-            }
-        }
-        return cnt;
+        // Deque<Node> stk = new ArrayDeque<>();
+        // stk.push(new Node(root, root.val));
+        // int cnt = 0;
+        // while (!stk.isEmpty()) {
+        //     Node cur = stk.pop();
+        //     cnt += cur.tn.val >= cur.currentMax ? 1 : 0;
+        //     for (TreeNode kid : new TreeNode[]{cur.tn.left, cur.tn.right}) {
+        //         if (kid != null) {
+        //             stk.push(new Node(kid, cur.currentMax));
+        //         }
+        //     }
+        // }
+        // return cnt;
+        
+        return preorder(root, root.val);
+    }
+    private int preorder(TreeNode n, int v) {
+        if (n == null) // base cases.
+            return 0;
+        int max = Math.max(n.val, v); // maximum so far on the path.
+        return (n.val >= v ? 1 : 0) + preorder(n.left, max) + preorder(n.right, max); // recurse to children.
     }
 }
