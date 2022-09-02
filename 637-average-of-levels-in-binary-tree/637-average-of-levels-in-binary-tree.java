e0a2f9912273515b96a1c7a1ae061556d86c1c9b
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-02 18:58:40 +0530

    Time: 2 ms (98.49%), Space: 43.2 MB (99.80%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -15,19 +15,36 @@
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> ans = new ArrayList<>();
-        List<List<Integer>> t = levelOrder(root);
-        for(List<Integer> i : t){
-            double sum = 0;
-            for(int j : i){
-                sum += j;
-            }
-            sum /= i.size();
-            ans.add(sum);
-        }
-        return ans;
+        // List<Double> ans = new ArrayList<>();
+        // List<List<Integer>> t = levelOrder(root);
+        // for(List<Integer> i : t){
+        //     double sum = 0;
+        //     for(int j : i)
+        //         sum += j;
+        //     sum /= i.size();
+        //     ans.add(sum);
+        // }
+        // return ans;
         
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if(root == null) return result;
+        q.add(root);
+        while(!q.isEmpty()) {
+            int n = q.size();
+            double sum = 0.0;
+            for(int i = 0; i < n; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            result.add(sum / n);
+        }
+        return result;
     }
+    
     public List<List<Integer>> levelOrder(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         List<List<Integer>> ans = new LinkedList<List<Integer>>();
