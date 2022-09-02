49487a01a3bf895af8e65a0bb5476eda8877bd12
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-02 18:59:05 +0530

    Time: 7 ms (9.12%), Space: 48.4 MB (12.91%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -45,21 +45,21 @@ class Solution {
         return result;
     }
     
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        List<List<Integer>> ans = new LinkedList<List<Integer>>();
-        if(root == null) return ans;
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelNum = queue.size();
-            List<Integer> subList = new LinkedList<Integer>();
-            for(int i = 0; i < levelNum; i++) {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                subList.add(queue.poll().val);
-            }
-            ans.add(subList);
-        }
-        return ans;
-    }
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    //     List<List<Integer>> ans = new LinkedList<List<Integer>>();
+    //     if(root == null) return ans;
+    //     queue.offer(root);
+    //     while(!queue.isEmpty()){
+    //         int levelNum = queue.size();
+    //         List<Integer> subList = new LinkedList<Integer>();
+    //         for(int i = 0; i < levelNum; i++) {
+    //             if(queue.peek().left != null) queue.offer(queue.peek().left);
+    //             if(queue.peek().right != null) queue.offer(queue.peek().right);
+    //             subList.add(queue.poll().val);
+    //         }
+    //         ans.add(subList);
+    //     }
+    //     return ans;
+    // }
 }
