005aa493727b19542d6b30a40ab962f4c1173d01
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-03 13:32:32 +0530

    Time: 4 ms (65.31%), Space: 42.2 MB (87.81%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,17 +1,42 @@
 class Solution {
-    public int[] numsSameConsecDiff(int n, int k) {
-        List<Integer> cur = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        for (int i = 2; i <= n; ++i) {
-            List<Integer> cur2 = new ArrayList<>();
-            for (int x : cur) {
-                int y = x % 10;
-                if (y + k < 10)
-                    cur2.add(x * 10 + y + k);
-                if (k > 0 && y - k >= 0)
-                    cur2.add(x * 10 + y - k);
-            }
-            cur = cur2;
+    public int[] numsSameConsecDiff(int N, int K) {
+        // List<Integer> cur = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        // for (int i = 2; i <= n; ++i) {
+        //     List<Integer> cur2 = new ArrayList<>();
+        //     for (int x : cur) {
+        //         int y = x % 10;
+        //         if (y + k < 10)
+        //             cur2.add(x * 10 + y + k);
+        //         if (k > 0 && y - k >= 0)
+        //             cur2.add(x * 10 + y - k);
+        //     }
+        //     cur = cur2;
+        // }
+        // return cur.stream().mapToInt(j->j).toArray();
+        
+        if (N == 1)
+            return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> results = new ArrayList<Integer>();
+        for (int num = 1; num < 10; ++num)
+            this.DFS(N - 1, num, K, results);
+        return results.stream().mapToInt(i->i).toArray();
+    }
+
+    protected void DFS(int N, int num, int K, List<Integer> results) {
+        if (N == 0) {
+            results.add(num);
+            return;
         }
-        return cur.stream().mapToInt(j->j).toArray();
+        List<Integer> nextDigits = new ArrayList<>();
+        Integer tailDigit = num % 10;
+        nextDigits.add(tailDigit + K);
+        if (K != 0)
+            nextDigits.add(tailDigit - K);
+        for (Integer nextDigit : nextDigits) {
+            if (0 <= nextDigit && nextDigit < 10) {
+                Integer newNum = num * 10 + nextDigit;
+                this.DFS(N - 1, newNum, K, results);
+            }
+        }
     }
 }
