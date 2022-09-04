75bc76276f1cbb2adb66099687867b86324bb697
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-04 19:30:22 +0530

    Time: 1 ms (92.01%), Space: 42.6 MB (15.80%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String sortSentence(String s) {
+        String str[] = s.split(" ");
+        String temp[] = new String[str.length];
+        int n = str.length;
+        StringBuilder sb = new StringBuilder();
+        for(String s1 : str){
+            temp[s1.charAt(s1.length() - 1) - '1'] = s1.substring(0, s1.length() - 1);
+        }
+        for(int i = 0; i < n - 1; i++)
+            sb.append(temp[i]).append(" ");
+        sb.append(temp[n - 1]);
+        return sb.toString();
+    }
+}
