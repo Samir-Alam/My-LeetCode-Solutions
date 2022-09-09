56eef73add7da8afbb320e7ef03e5d36b9634bb6
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-09 18:40:12 +0530

    Time: 21 ms (7.92%), Space: 53.2 MB (83.08%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,25 +1,43 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        // Map<Integer, List<Integer>> map = new HashMap<>();
+        // int n = groupSizes.length;
+        // for(int i = 0; i < n; i++){
+        //     List<Integer> temp = map.getOrDefault(groupSizes[i], new ArrayList<Integer>());
+        //     temp.add(i);
+        //     map.put(groupSizes[i], temp);
+        // }
+        // List<List<Integer>> ans = new ArrayList<>();
+        // Set<Integer> set = new HashSet<>();
+        // for(int i : groupSizes)
+        //     set.add(i);
+        // for(int i : set){
+        //     if(map.get(i).size() > i){
+        //         int size = map.get(i).size();
+        //         for(int j = 0; j < size; j += i)
+        //             ans.add(map.get(i).subList(j, j + i));
+        //     }else
+        //         ans.add(map.get(i));
+        // }
+        // return ans;
+        
+        //2nd solution
         int n = groupSizes.length;
-        for(int i = 0; i < n; i++){
-            List<Integer> temp = map.getOrDefault(groupSizes[i], new ArrayList<Integer>());
-            temp.add(i);
-            map.put(groupSizes[i], temp);
-        }
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
         List<List<Integer>> ans = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        for(int i : groupSizes)
-            set.add(i);
-        for(int i : set){
-            if(map.get(i).size() > i){
-                int size = map.get(i).size();
-                for(int j = 0; j < size; j += i)
-                    ans.add(map.get(i).subList(j, j + i));
-            }else{
-                ans.add(map.get(i));
+        
+        for(int i = 0; i < n; i++){
+            int curr = groupSizes[i];
+            List<Integer> temp = new ArrayList<>();
+            if(map.containsKey(curr)) temp = map.get(curr);
+            temp.add(i);
+            map.put(curr, temp);
+            if(temp.size() == curr){
+                ans.add(temp);
+                map.remove(curr);
             }
         }
+        
         return ans;
     }
 }
