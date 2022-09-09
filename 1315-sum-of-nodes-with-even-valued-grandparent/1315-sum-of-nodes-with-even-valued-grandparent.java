a2b238999b6ffd120ab1d4d9dd1a0d5c1488a4cf
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-09 20:35:20 +0530

    Time: 1 ms (100.00%), Space: 52.6 MB (40.28%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -14,25 +14,42 @@
  * }
  */
 class Solution {
-    int res = 0;
+    // int res = 0;
+    // public int sumEvenGrandparent(TreeNode root) {
+    //     if(root == null)
+    //         return 0;
+    //     gc(root);
+    //     return res;
+    // }
+    // public void gc(TreeNode root){
+    //     if(root == null)
+    //         return;
+    //     if(root.val % 2 == 0 && root.left != null){
+    //         res += root.left.left != null ? root.left.left.val : 0;
+    //         res += root.left.right != null ? root.left.right.val : 0;
+    //     }
+    //     gc(root.left);
+    //     if(root.val % 2 == 0 && root.right != null){
+    //         res += root.right.left != null ? root.right.left.val : 0;
+    //         res += root.right.right != null ? root.right.right.val : 0;
+    //     }
+    //     gc(root.right);
+    // } 
+    
+    //2nd Solution
+    public int sum = 0;
+
     public int sumEvenGrandparent(TreeNode root) {
-        if(root == null)
-            return 0;
-        gc(root);
-        return res;
+        dfs(root, null, null);
+        return sum;
     }
-    public void gc(TreeNode root){
-        if(root == null)
-            return;
-        if(root.val % 2 == 0 && root.left != null){
-            res += root.left.left != null ? root.left.left.val : 0;
-            res += root.left.right != null ? root.left.right.val : 0;
+
+    void dfs(TreeNode current, TreeNode parent, TreeNode grandParent) {
+        if (current == null) return; // base case 
+        if (grandParent != null && grandParent.val % 2 == 0) {
+            sum += current.val;
         }
-        gc(root.left);
-        if(root.val % 2 == 0 && root.right != null){
-            res += root.right.left != null ? root.right.left.val : 0;
-            res += root.right.right != null ? root.right.right.val : 0;
-        }
-        gc(root.right);
-    } 
+        dfs(current.left, current, parent);// ( newChild, parent, GrandParent)
+        dfs(current.right, current, parent);
+    }
 }
