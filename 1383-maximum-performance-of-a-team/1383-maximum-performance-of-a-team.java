acc47816b76d9f95b564dfbd703018da4309f38f
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-11 11:12:45 +0530

    Time: 102 ms (25.67%), Space: 69.2 MB (47.33%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,21 +1,37 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int MOD = (int) (1e9 + 7);
+//         int MOD = (int) (1e9 + 7);
+//         int[][] engineers = new int[n][2];
+//         for (int i = 0; i < n; ++i) 
+//             engineers[i] = new int[] {efficiency[i], speed[i]};
+
+//         Arrays.sort(engineers, (a, b) -> b[0] - a[0]);  //sorting in descending order
+
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);    ////sorting in ascending order
+//         long res = Long.MIN_VALUE, totalSpeed = 0;
+
+//         for (int[] engineer : engineers) {
+//             if (pq.size() == k) totalSpeed -= pq.poll();  // layoff the one with min speed
+//             pq.add(engineer[1]);
+//             totalSpeed = (totalSpeed + engineer[1]);
+//             res = Math.max(res, (totalSpeed * engineer[0]));  // min efficiency is the efficiency of new engineer
+//         }
+//         return (int) (res % MOD);
+        
+        int mod = (int)(1e9 + 7);
         int[][] engineers = new int[n][2];
-        for (int i = 0; i < n; ++i) 
+        for(int i = 0; i < n; ++i)
             engineers[i] = new int[] {efficiency[i], speed[i]};
-
-        Arrays.sort(engineers, (a, b) -> b[0] - a[0]);  //sorting in descending order
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);    ////sorting in ascending order
-        long res = Long.MIN_VALUE, totalSpeed = 0;
-
-        for (int[] engineer : engineers) {
-            if (pq.size() == k) totalSpeed -= pq.poll();  // layoff the one with min speed
+        Arrays.sort(engineers, (a, b) -> b[0] - a[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
+        long res = Long.MIN_VALUE, sum = 0;
+        for(int engineer[] : engineers){
+            if(pq.size() == k)
+                sum -= pq.poll();
             pq.add(engineer[1]);
-            totalSpeed = (totalSpeed + engineer[1]);
-            res = Math.max(res, (totalSpeed * engineer[0]));  // min efficiency is the efficiency of new engineer
+            sum +=engineer[1];
+            res = Math.max(res, (sum * engineer[0]));
         }
-        return (int) (res % MOD);
+        return (int)(res % mod);
     }
 }
