9c12c716611d18a89bbc1ef87bd1c6bc7fd18b86
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-16 20:56:32 +0530

    Time: 9 ms (61.15%), Space: 167.3 MB (61.82%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,19 +1,35 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int n = nums.length;
+        // int n = nums.length;
+        // int[] ans = new int[n];
+        // int left = 0, right = n - 1;
+        // for (int i = 0, j = n - 1; i < n; ++i, --j) {
+        //     if (nums[i] < pivot) {
+        //         ans[left++] = nums[i];
+        //     }
+        //     if (nums[j] > pivot) {
+        //         ans[right--] = nums[j];
+        //     }
+        // }
+        // while (left <= right) {
+        //     ans[left++] = pivot;
+        // }
+        // return ans;
+        
+        //2nd Solution
+        int n = nums.length, pivotFreq = 0, i = 0;
         int[] ans = new int[n];
-        int left = 0, right = n - 1;
-        for (int i = 0, j = n - 1; i < n; ++i, --j) {
-            if (nums[i] < pivot) {
-                ans[left++] = nums[i];
-            }
-            if (nums[j] > pivot) {
-                ans[right--] = nums[j];
-            }
+        for (int num : nums) {
+            if (num < pivot)
+                ans[i++] = num;
+            else if (num == pivot)
+                ++pivotFreq;
         }
-        while (left <= right) {
-            ans[left++] = pivot;
-        }
+        while (pivotFreq-- > 0)
+            ans[i++] = pivot;
+        for (int num : nums)
+            if (num > pivot)
+                ans[i++] = num;
         return ans;
     }
 }
