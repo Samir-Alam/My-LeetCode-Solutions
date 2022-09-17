d51bfa8ccec30d6ceb68c3b0f55d208f915ef5d2
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-17 16:57:09 +0530

    Time: 2327 ms (12.10%), Space: 52.3 MB (98.42%) - LeetHub

---
+++ 336-palindrome-pairs/336-palindrome-pairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> pairs = new LinkedList<>();
+        if (words == null) return pairs;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; ++ i) map.put(words[i], i);
+        for (int i = 0; i < words.length; ++ i) {
+            int l = 0, r = 0;
+            while (l <= r) {
+                String s = words[i].substring(l, r);
+                Integer j = map.get(new StringBuilder(s).reverse().toString());
+                if (j != null && i != j && isPalindrome(words[i].substring(l == 0 ? r : 0, l == 0 ? words[i].length() : l)))
+                    pairs.add(Arrays.asList(l == 0 ? new Integer[]{i, j} : new Integer[]{j, i}));
+                if (r < words[i].length()) ++r;
+                else ++l;
+            }
+        }
+        return pairs;
+    }
+
+    private boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length()/2; ++ i)
+            if (s.charAt(i) != s.charAt(s.length()-1-i))
+                return false;
+        return true;
+    }
+}
