cbefc976ffc492a09d8fd623d34bcd5b01bbea05
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-18 16:05:41 +0530

    Time: 0 ms (100.00%), Space: 79.2 MB (42.25%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,5 +1,8 @@
 class Solution {
     public int findCenter(int[][] edges) {
+        
+        //every edges has to connect to the center node,
+        //so you just need to find the common node which is connected to every node
         if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
             return edges[0][0];
         else
