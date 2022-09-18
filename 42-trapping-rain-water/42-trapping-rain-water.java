d3991dd14e5d62a58fb07e13eec61d0fb35db42d
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-18 09:18:17 +0530

    Time: 1 ms (99.76%), Space: 49 MB (34.50%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,20 +1,42 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length, ans = 0;
-        int left[] = new int[n];
-        int right[] = new int[n];
+//         int n = height.length, ans = 0;
+//         int left[] = new int[n];
+//         int right[] = new int[n];
         
-        left[0] = height[0];
-        for(int i = 1; i < n; i++)
-            left[i] = Math.max(left[i - 1], height[i]);
+//         left[0] = height[0];
+//         for(int i = 1; i < n; i++)
+//             left[i] = Math.max(left[i - 1], height[i]);
         
-        right[n - 1] = height[n - 1];
-        for(int i = n - 2; i >= 0; i--)
-            right[i] = Math.max(right[i + 1], height[i]);
+//         right[n - 1] = height[n - 1];
+//         for(int i = n - 2; i >= 0; i--)
+//             right[i] = Math.max(right[i + 1], height[i]);
         
-        for(int i = 0; i < n; i++)
-            ans += Math.min(left[i], right[i]) - height[i];
+//         for(int i = 0; i < n; i++)
+//             ans += Math.min(left[i], right[i]) - height[i];
         
+//         return ans;
+        
+        //2nd Solution - O(1) Space
+        
+        if (height.length <= 2) return 0;
+        int n = height.length, maxLeft = height[0], maxRight = height[n-1];
+        int left = 1, right = n - 2, ans = 0;
+        while (left <= right) {
+            if (maxLeft < maxRight) {
+                if (height[left] > maxLeft)
+                    maxLeft = height[left];
+                else
+                    ans += maxLeft - height[left];
+                left += 1;
+            } else {
+                if (height[right] > maxRight)
+                    maxRight = height[right];
+                else
+                    ans += maxRight - height[right];
+                right -= 1;
+            }
+        }
         return ans;
     }
 }
