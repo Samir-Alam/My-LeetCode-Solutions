6cce56afbc2b44aa8b647bf8ab9017cdc142aee4
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-19 15:10:28 +0530

    Time: 1 ms (54.92%), Space: 40.8 MB (66.88%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,15 +1,18 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int matches = 0;
-        while(n != 1){
-            if(n % 2 == 0){
-                matches += n / 2;
-                n /= 2;
-            }else{
-                matches += n / 2;
-                n = (n / 2) + 1;
-            }
-        }
-        return matches;
+        // int matches = 0;
+        // while(n != 1){
+        //     if(n % 2 == 0){
+        //         matches += n / 2;
+        //         n /= 2;
+        //     }else{
+        //         matches += n / 2;
+        //         n = (n / 2) + 1;
+        //     }
+        // }
+        // return matches;
+        
+        //2nd Solution
+        return n - 1;
     }
 }
