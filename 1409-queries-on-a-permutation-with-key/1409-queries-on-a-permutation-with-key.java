411f7bc0b0ae3b29d8051f7400e2430b05eadf89
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-21 19:34:19 +0530

    Time: 21 ms (26.14%), Space: 43.3 MB (53.79%) - LeetHub

--- 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.java
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] processQueries(int[] queries, int m) {
-        List<Integer> m_arr = new ArrayList<>();
-        int final_arr[] = new int[queries.length];
-        for(int i=1;i<=m;i++) 
-            m_arr.add(i);
-
-        for(int i=0;i<queries.length;i++)
-        {
-            int temp = queries[i];
-
-                if(m_arr.contains(temp)){
-                    final_arr[i] = m_arr.indexOf(temp);
-                    m_arr.remove(m_arr.indexOf(temp));
-                    m_arr.add(0,temp);
-            }
+        // List<Integer> m_arr = new ArrayList<>();
+        // int final_arr[] = new int[queries.length];
+        // for(int i=1;i<=m;i++) 
+        //     m_arr.add(i);
+        // for(int i=0;i<queries.length;i++){
+        //     int temp = queries[i];
+        //         if(m_arr.contains(temp)){
+        //             final_arr[i] = m_arr.indexOf(temp);
+        //             m_arr.remove(m_arr.indexOf(temp));
+        //             m_arr.add(0,temp);
+        //     }
+        // }
+        // return final_arr;
+        
+        LinkedList<Integer> P = new LinkedList<>();
+        for (int i=1; i<=m;i++)
+            P.add(i);
+        int[] res = new int[queries.length];
+        for (int i=0; i<queries.length; i++) {
+            int q = queries[i];
+            int idx = P.indexOf(q);
+            int val = P.get(idx);
+            P.remove(idx);
+            P.addFirst(val);
+            res[i] = idx;
         }
-
-        return final_arr;
+        return res;
     }
 }
