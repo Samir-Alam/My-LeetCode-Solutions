79ba4fbbd7d8715b756bfa7a6cc4f2a5b8b05a32
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-26 21:11:20 +0530

    Time: 2 ms (69.35%), Space: 42.9 MB (25.35%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[] uf = new int[26];
+    public boolean equationsPossible(String[] equations) {
+        for (int i = 0; i < 26; ++i) uf[i] = i;
+        for (String e : equations)
+            if (e.charAt(1) == '=')
+                uf[find(e.charAt(0) - 'a')] = find(e.charAt(3) - 'a');
+        for (String e : equations)
+            if (e.charAt(1) == '!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a'))
+                return false;
+        return true;
+    }
+
+    public int find(int x) {
+        if (x != uf[x]) uf[x] = find(uf[x]);
+        return uf[x];
+    }
+}
