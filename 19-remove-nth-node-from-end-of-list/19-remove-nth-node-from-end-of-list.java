c1e94f8f60ef5fd18811d293415f211a27ee0e36
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-09-28 16:06:52 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (89.75%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -10,39 +10,39 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // int size = 0;
-        // ListNode temp = head;
-        // if(head.next == null){
-        //     head = head.next;
-        //     return head;
-        // }
-        // ListNode first = head, second = head;
-        // for (int i = 0; i < n; i++) {
-        //     if (second.next == null) {
-        //         if (i == n - 1)
-        //             head = head.next;
-        //         return head;
-        //     }
-        //     second = second.next;
-        // }
-        // while(second.next != null){
-        //     first = first.next;
-        //     second = second.next;
-        // }
-        // first.next = first.next.next;
-        // return head;
+        int size = 0;
+        ListNode temp = head;
+        if(head.next == null){
+            head = head.next;
+            return head;
+        }
+        ListNode first = head, second = head;
+        for (int i = 0; i < n; i++) {
+            if (second.next == null) {
+                if (i == n - 1)
+                    head = head.next;
+                return head;
+            }
+            second = second.next;
+        }
+        while(second.next != null){
+            first = first.next;
+            second = second.next;
+        }
+        first.next = first.next.next;
+        return head;
         
         //2nd solution
-        ListNode start = new ListNode(0);
-        ListNode slow = start, fast = start;
-        slow.next = head;
-        for(int i = 1; i <= n + 1; i++)
-            fast = fast.next;
-        while(fast != null) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        slow.next = slow.next.next;
-        return start.next;
+        // ListNode start = new ListNode(0);
+        // ListNode slow = start, fast = start;
+        // slow.next = head;
+        // for(int i = 1; i <= n + 1; i++)
+        //     fast = fast.next;
+        // while(fast != null) {
+        //     slow = slow.next;
+        //     fast = fast.next;
+        // }
+        // slow.next = slow.next.next;
+        // return start.next;
     }
 }
