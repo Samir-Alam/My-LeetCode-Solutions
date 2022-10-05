bb3e2fc79e85fb4db53a6218b6d09ef672f535f8
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-05 09:22:35 +0530

    Time: 3 ms (5.62%), Space: 42.4 MB (85.45%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -14,25 +14,62 @@
  * }
  */
 class Solution {
-    public TreeNode addOneRow(TreeNode root, int val, int depth) {
-        if(depth == 1){
-            TreeNode newRoot = new TreeNode(val);
-            newRoot.left = root;
-            return newRoot;
+    // public TreeNode addOneRow(TreeNode root, int val, int depth) {
+    //     if(depth == 1){
+    //         TreeNode newRoot = new TreeNode(val);
+    //         newRoot.left = root;
+    //         return newRoot;
+    //     }
+    //     dfs(root, val, depth);
+    //     return root;
+    // }
+    // public void dfs(TreeNode root, int val, int depth){
+    //     if(depth - 1 == 1){
+    //         TreeNode left = new TreeNode(val), right = new TreeNode(val);
+    //         left.left = root.left;
+    //         right.right = root.right;
+    //         root.left = left;
+    //         root.right = right;
+    //     }if(root.left != null)
+    //         dfs(root.left, val, depth - 1);
+    //     if(root.right != null)
+    //         dfs(root.right, val,depth - 1);
+    // }
+    
+    class Node{
+        Node(TreeNode n,int d){
+            node=n;
+            depth=d;
         }
-        dfs(root, val, depth);
-        return root;
+        TreeNode node;
+        int depth;
     }
-    public void dfs(TreeNode root, int val, int depth){
-        if(depth - 1 == 1){
-            TreeNode left = new TreeNode(val), right = new TreeNode(val);
-            left.left = root.left;
-            right.right = root.right;
-            root.left = left;
-            root.right = right;
-        }if(root.left != null)
-            dfs(root.left, val, depth - 1);
-        if(root.right != null)
-            dfs(root.right, val,depth - 1);
+    public TreeNode addOneRow(TreeNode t, int v, int d) {
+        if (d == 1) {
+            TreeNode n = new TreeNode(v);
+            n.left = t;
+            return n;
+        } 
+        Stack<Node> stack=new Stack<>();
+        stack.push(new Node(t,1));
+        while(!stack.isEmpty())
+        {
+            Node n=stack.pop();
+            if(n.node==null)
+                continue;
+            if (n.depth == d - 1 ) {
+                TreeNode temp = n.node.left;
+                n.node.left = new TreeNode(v);
+                n.node.left.left = temp;
+                temp = n.node.right;
+                n.node.right = new TreeNode(v);
+                n.node.right.right = temp;
+                
+            } else{
+                stack.push(new Node(n.node.left, n.depth + 1));
+                stack.push(new Node(n.node.right, n.depth + 1));
+            }
+        }
+        return t;
     }
 }
