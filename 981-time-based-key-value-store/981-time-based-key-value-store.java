812a97802b4f39417dccc2813553e0db066313b4
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-06 10:02:18 +0530

    Time: 439 ms (13.86%), Space: 224.6 MB (10.10%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,25 +1,46 @@
 class TimeMap {
 
-    HashMap<String, HashMap<Integer, String>> keyTimeMap;
+    // HashMap<String, HashMap<Integer, String>> keyTimeMap;
+    // public TimeMap() {
+    //     keyTimeMap = new HashMap<String, HashMap<Integer, String>>();
+    // }
+    // public void set(String key, String value, int timestamp) {
+    //     if (!keyTimeMap.containsKey(key)) {
+    //         keyTimeMap.put(key, new HashMap<Integer, String>());
+    //     }
+    //     keyTimeMap.get(key).put(timestamp, value);
+    // }
+    // public String get(String key, int timestamp) {
+    //     if (!keyTimeMap.containsKey(key)) {
+    //         return "";
+    //     }
+    //     for (int currTime = timestamp; currTime >= 1; --currTime) {
+    //         if (keyTimeMap.get(key).containsKey(currTime)) {
+    //             return keyTimeMap.get(key).get(currTime);
+    //         }
+    //     }
+    //     return ""; 
+    // }
+    
+    //2nd solution
+    Map<String, TreeMap<Integer,String>> map;
     public TimeMap() {
-        keyTimeMap = new HashMap<String, HashMap<Integer, String>>();
+        this.map=new HashMap<>();
     }
+    
     public void set(String key, String value, int timestamp) {
-        if (!keyTimeMap.containsKey(key)) {
-            keyTimeMap.put(key, new HashMap<Integer, String>());
+        if(!this.map.containsKey(key)){
+            TreeMap<Integer,String> tmap=new TreeMap<>();
+            tmap.put(timestamp,value);
+            this.map.put(key,tmap);
+        }else{
+            this.map.get(key).put(timestamp,value);
         }
-        keyTimeMap.get(key).put(timestamp, value);
     }
+    
     public String get(String key, int timestamp) {
-        if (!keyTimeMap.containsKey(key)) {
-            return "";
-        }
-        for (int currTime = timestamp; currTime >= 1; --currTime) {
-            if (keyTimeMap.get(key).containsKey(currTime)) {
-                return keyTimeMap.get(key).get(currTime);
-            }
-        }
-        return ""; 
+        if(!this.map.containsKey(key))return "";
+        return this.map.get(key).floorEntry(timestamp)!=null?     this.map.get(key).floorEntry(timestamp).getValue():"";
     }
 }
 
