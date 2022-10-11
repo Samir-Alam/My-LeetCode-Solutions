def0b0c45c6c27ef2a31368a3b1dd741af0f9e54
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-11 15:42:00 +0530

    Time: 5 ms (46.33%), Space: 94 MB (23.87%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -2,28 +2,28 @@ class Solution {
     public boolean increasingTriplet(int[] nums) {
         if (nums.length < 3)
             return false;
-      int seq = 1;
-      int min_num = nums[0];
-      int max_seq = Integer.MIN_VALUE;
-      int store_min = min_num;
-      for (int i = 1; i < nums.length; i++){
-        if (nums[i] == min_num)
-          continue;
-        else if (nums[i] < min_num){
-          min_num = nums[i];
-          continue;
+        int seq = 1;
+        int min_num = nums[0];
+        int max_seq = Integer.MIN_VALUE;
+        int store_min = min_num;
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] == min_num)
+                continue;
+            else if (nums[i] < min_num){
+                min_num = nums[i];
+                continue;
+            }
+            else if (nums[i] < max_seq) {
+                max_seq = nums[i];
+                store_min = min_num;            
+            } 
+            else if (nums[i] > max_seq){ 
+                seq++;
+                if (seq == 3)
+                    return true;
+                max_seq = nums[i];
+            }
         }
-        else if (nums[i] < max_seq) {
-          max_seq = nums[i];
-          store_min = min_num;            
-        } 
-        else if (nums[i] > max_seq){ 
-            seq++;
-            if (seq == 3)
-                return true;
-          max_seq = nums[i];
-        }
-      }
-      return false;
+        return false;
     }
 }
