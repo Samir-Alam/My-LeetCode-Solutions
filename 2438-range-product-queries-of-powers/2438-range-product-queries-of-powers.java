ccf7bd9ab69fdb3169979c66df8584b7cfdc589c
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-15 21:41:59 +0530

    Time: 94 ms (16.67%), Space: 136.7 MB (16.67%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
@@ -20,30 +20,24 @@ class Solution {
         // }
         // return res;
         
-        //2nd Solution
         int mod=1000000007;
-        //generating the array based on the given n value  n%2 and n/2 evry time genrating an array of 0 and 1 indcating whether to consdier the index or not and generate the powers array according to the array generated indexes
-        List<Integer> arr=new ArrayList<Integer>();
-        List<Integer> powers=new ArrayList<Integer>();
-        while(n>0){  
-            arr.add((int)n%2);
-            n/=2;
+        List<Integer> arr = new ArrayList<Integer>();
+        List<Integer> powers = new ArrayList<Integer>();
+        while(n > 0){
+            arr.add((int)n % 2);
+            n /= 2;
         }
-         //System.out.print(arr);
-        for(int i=0;i<arr.size();i++){
-            if(arr.get(i)==1){  //considering only the arrays of value equals 1 indicating the maximum powers sum to n
-                int t=(int)Math.pow(2,i);
+        for(int i = 0; i < arr.size(); i++)
+            if(arr.get(i) == 1){
+                int t = (int)Math.pow(2, i);
                 powers.add(t);
             }
-        }
-        int result[]=new int[queries.length];
-        int j=0;
-        for(int []a:queries){
-            long res=1;
-            for(int i=a[0];i<=a[1];i++){
-                res=(res*powers.get(i)%mod);
-            }
-            result[j++]=(int)res%mod;
+        int result[] = new int[queries.length], j = 0;
+        for(int []a : queries){
+            long res = 1;
+            for(int i = a[0]; i <= a[1]; i++)
+                res = (res * powers.get(i) % mod);
+            result[j++] = (int)res % mod;
         }
         return result;
     }
