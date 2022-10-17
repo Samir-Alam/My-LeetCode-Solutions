3f393bfc88547bb7c7a6c36fe62c7a024829b5f5
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-17 15:37:49 +0530

    Time: 18 ms (92.39%), Space: 51.3 MB (94.25%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
@@ -1,22 +1,32 @@
 class Solution {
     public int[] findingUsersActiveMinutes(int[][] logs, int k) {
-        int ans[] = new int[k];
-        Map<Integer, HashSet<Integer>> map = new HashMap<>();
-        for(int i[] : logs){
-            HashSet set = new HashSet<>();
-            if(map.containsKey(i[0])){
-                set = map.get(i[0]);
-                set.add(i[1]);
-                map.put(i[0], set);
-            }
-            else{
-                set.add(i[1]);
-                map.put(i[0], set);
-            }
+        // int ans[] = new int[k];
+        // Map<Integer, HashSet<Integer>> map = new HashMap<>();
+        // for(int i[] : logs){
+        //     HashSet set = new HashSet<>();
+        //     if(map.containsKey(i[0])){
+        //         set = map.get(i[0]);
+        //         set.add(i[1]);
+        //         map.put(i[0], set);
+        //     }
+        //     else{
+        //         set.add(i[1]);
+        //         map.put(i[0], set);
+        //     }
+        // }
+        // for (Integer i : map.keySet())
+        //     ans[map.get(i).size() - 1]++;
+        // return ans;
+        
+        //2nd Solution From Discussion
+        int[] res = new int[k];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] l : logs) {
+            map.putIfAbsent(l[0], new HashSet<>());
+            map.get(l[0]).add(l[1]);
         }
-        for (Integer i : map.keySet()){
-            ans[map.get(i).size() - 1]++;
-        }
-        return ans;
+        for (int key : map.keySet())
+            res[map.get(key).size() - 1]++;
+        return res;
     }
 }
