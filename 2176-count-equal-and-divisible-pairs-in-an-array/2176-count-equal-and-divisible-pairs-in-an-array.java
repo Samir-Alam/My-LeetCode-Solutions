86dbefe0d0edb69d731c24558ef5dd1a9d940bd5
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-17 17:37:01 +0530

    Time: 10 ms (20.57%), Space: 43.8 MB (13.29%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.java
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.java
@@ -10,15 +10,33 @@ class Solution {
         // return ans;
         
         //2nd Solution From Discussion
-        Map<Integer, List<Integer>> indices = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i)
-            indices.computeIfAbsent(nums[i], l -> new ArrayList<>()).add(i);
-        int cnt = 0;
-        for (List<Integer> ind : indices.values()) 
-            for (int i = 0; i < ind.size(); ++i) 
-                for (int j = 0; j < i; ++j) 
-                    if (ind.get(i) * ind.get(j) % k == 0) 
-                        ++cnt;
-        return cnt;
+        // Map<Integer, List<Integer>> indices = new HashMap<>();
+        // for (int i = 0; i < nums.length; ++i)
+        //     indices.computeIfAbsent(nums[i], l -> new ArrayList<>()).add(i);
+        // int cnt = 0;
+        // for (List<Integer> ind : indices.values()) 
+        //     for (int i = 0; i < ind.size(); ++i) 
+        //         for (int j = 0; j < i; ++j) 
+        //             if (ind.get(i) * ind.get(j) % k == 0) 
+        //                 ++cnt;
+        // return cnt;
+        
+        HashMap<Integer,List<Integer>> hMap = new HashMap<>();
+        int count = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(!hMap.containsKey(nums[i])){
+                List<Integer> l = new ArrayList<>();
+                l.add(i);
+                hMap.put(nums[i],l);
+            }else{
+                List<Integer> v = hMap.get(nums[i]);
+                for(Integer j : v){
+                    if((i*j)%k == 0) count++;
+                }
+                v.add(i);
+                hMap.put(nums[i],v);                
+            }
+        }
+        return count;
     }
 }
