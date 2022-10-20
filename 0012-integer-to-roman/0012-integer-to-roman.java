df07515990ee18d6badfcc3a366409aaa8d79892
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-20 07:49:08 +0530

    Time: 13 ms (43.12%), Space: 45.4 MB (62.02%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -6,17 +6,16 @@ class Solution {
         // String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         // return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
         
+        
+        //2nd Solution
         int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
         StringBuilder sb = new StringBuilder();
-
-        for(int i=0;i<values.length;i++) {
+        for(int i=0;i<values.length;i++)
             while(num >= values[i]) {
                 num -= values[i];
                 sb.append(strs[i]);
             }
-        }
         return sb.toString();
     }
 }
