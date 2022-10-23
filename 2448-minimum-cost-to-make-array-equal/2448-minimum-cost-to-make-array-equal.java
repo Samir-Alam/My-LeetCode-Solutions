b00308d211fa0dd3cb5b9089bc5c12d35039be02
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-23 12:34:17 +0530

    Time: 98 ms (100.00%), Space: 77.8 MB (100.00%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long minCost(int[] nums, int[] cost) {
+    // public long minCost(int[] nums, int[] cost) {
         // long ans = 0;
         // Map<Integer,Integer> map = new HashMap<>();
         // int min = Integer.MAX_VALUE, max = 0;
@@ -48,35 +48,61 @@ class Solution {
         // }
         // return minCost;
         
-        int lo = Integer.MAX_VALUE, hi = Integer.MIN_VALUE;
-        for(int val : nums){
-            lo = Math.min(lo, val);
-            hi= Math.max(hi, val);
-        }
-        //System.out.println(lo + " " + hi);
-        long res = (long)(1e18);
-        while(lo <= hi){
-            int mid = (lo + hi) >> 1;
-            res = Math.min(res, change(nums, cost, mid));
-            long left = change(nums, cost, mid-1); // this two steps are done to check which side should we go for further answer.
-            long right = change(nums, cost, mid+1); // if left is smaller then the possible answer should lie in the left part or vise versa
-            if(left < right){
-                hi = mid-1;
-            }else{
-                lo = mid+1;
-            }
-        }
-        return res;
-    }
+    //     int lo = Integer.MAX_VALUE, hi = Integer.MIN_VALUE;
+    //     for(int val : nums){
+    //         lo = Math.min(lo, val);
+    //         hi= Math.max(hi, val);
+    //     }
+    //     long res = (long)(1e18);
+    //     while(lo <= hi){
+    //         int mid = (lo + hi) >> 1;
+    //         res = Math.min(res, change(nums, cost, mid));
+    //         long left = change(nums, cost, mid-1);
+    //         long right = change(nums, cost, mid+1);
+    //         if(left < right){
+    //             hi = mid - 1;
+    //         }else
+    //             lo = mid + 1;
+    //     }
+    //     return res;
+    // }
+    // public long change(int[] nums, int[] costs, int tar){
+    //     long ans = 0;
+    //     for(int i = 0; i<nums.length; i++)
+    //         if(nums[i] != tar){
+    //             int diff = Math.abs(nums[i] - tar); 
+    //             ans+=(long) diff*costs[i];
+    //         }
+    //     return ans;
+    // }
     
-    public long change(int[] nums, int[] costs, int tar){
-        long ans = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] != tar){
-                int diff = Math.abs(nums[i] - tar); 
-                ans+=(long) diff*costs[i]; // I just forget to typecast long here, which cost me extra 5min
-            }
+    public long check(int mid,int[] arr,int[] cost){
+    
+    long sum=0;
+        
+     for(int i=0;i<arr.length;i++){
+         sum+= 1l* Math.abs(mid-arr[i])*cost[i];
+     }       
+     return sum;
+}
+
+    public long minCost(int[] nums, int[] cost) {
+        int l=0,r=1000000;
+        while(l<=r){
+       int mid= (l+r)/2;
+         long p1=check(mid-1,nums,cost);
+        long p2=check(mid,nums,cost);
+        long p3= check(mid+1,nums,cost);
+       if(p1>=p2 && p3>=p2){
+           return p2;
+       }
+        else if(p1<p2){
+             r= (mid-1);
         }
-        return ans;
+         else  
+          l=(mid+1);
+    }
+        
+        return 0;
     }
 }
