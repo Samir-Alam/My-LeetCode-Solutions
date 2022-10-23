c5ffa7418994fdaa14b6f74d1922ef0d285688eb
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-23 12:29:54 +0530

    Time: 960 ms (25.00%), Space: 144 MB (75.00%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -1,26 +1,51 @@
 class Solution {
     public long minCost(int[] nums, int[] cost) {
-        long ans = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        int min = Integer.MAX_VALUE, max = 0;
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i], cost[i]+map.getOrDefault(nums[i], 0));
-            min = Math.min(min,nums[i]);
-            max = Math.max(max,nums[i]);
+        // long ans = 0;
+        // Map<Integer,Integer> map = new HashMap<>();
+        // int min = Integer.MAX_VALUE, max = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     map.put(nums[i], cost[i]+map.getOrDefault(nums[i], 0));
+        //     min = Math.min(min,nums[i]);
+        //     max = Math.max(max,nums[i]);
+        // }
+        // long cmin = map.get(min);
+        // long cmax = map.get(max);
+        // while(min<max){
+        //     if(cmin<=cmax){
+        //         ans += cmin;
+        //         min++;
+        //         cmin += map.getOrDefault(min,0);
+        //     }else{
+        //         ans += cmax;
+        //         max--;
+        //         cmax += map.getOrDefault(max,0);
+        //     }
+        // }
+        // return ans;
+        
+     TreeMap<Integer, Long> numCosts = new TreeMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            numCosts.put(nums[i], numCosts.getOrDefault(nums[i], (long) 0) + cost[i]);
         }
-        long cmin = map.get(min);
-        long cmax = map.get(max);
-        while(min<max){
-            if(cmin<=cmax){
-                ans += cmin;
-                min++;
-                cmin += map.getOrDefault(min,0);
-            }else{
-                ans += cmax;
-                max--;
-                cmax += map.getOrDefault(max,0);
-            }
+        long preSum = 0;  // exclusive
+        long postSum = 0;  // inclusive
+        for (int n : numCosts.keySet()) {
+            postSum += numCosts.get(n);
         }
-        return ans;
+        int target = numCosts.firstKey();
+        long currCost = 0;
+        for (int n : numCosts.keySet()) {
+            currCost += Math.abs(n - target) * numCosts.get(n);
+        }
+        long minCost = currCost;
+        while (numCosts.higherKey(target) != null) {
+            int nextTarget = numCosts.higherKey(target);
+            preSum += numCosts.get(target);
+            postSum -= numCosts.get(target);
+            currCost += Math.abs(nextTarget - target) * (preSum - postSum);
+            minCost = Math.min(minCost, currCost);
+            target = nextTarget;
+        }
+        return minCost;
     }
 }
