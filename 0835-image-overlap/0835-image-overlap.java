55c2f41e75461082c75260d936b868257e9aa0d2
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-27 22:18:40 +0530

    Time: 51 ms (58.79%), Space: 42.8 MB (78.79%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -25,13 +25,13 @@ class Solution {
         int C2 = img2[0].length;
         int max = Integer.MIN_VALUE;	
 		//Slide one image on other image.
-        for(int r=0; r<R1+R2-1; r++){
-            int i = R1-1 + Math.min(0, R2-1-r);
-            int x = r + Math.min(0, R2-1-r);
-            for(int c=0; c<C1+C2-1;  c++){
-                int j = C1-1 + Math.min( 0 ,  C2-1-c);
-                int y = c + Math.min(0 , C2-1-c);
-                max = Math.max(max, overlap(img1,img2,i,j,x,y));
+        for(int r = 0; r < R1 + R2 - 1; r++){
+            int i = R1 - 1 + Math.min(0, R2 - 1 - r);
+            int x = r + Math.min(0, R2 - 1 - r);
+            for(int c = 0; c < C1 + C2 - 1;  c++){
+                int j = C1 - 1 + Math.min(0 ,  C2 - 1 - c);
+                int y = c + Math.min(0, C2 - 1 - c);
+                max = Math.max(max, overlap(img1, img2, i, j, x, y));
             }
         }
         return max;
@@ -39,11 +39,10 @@ class Solution {
 	//Compares two matrices from give coordinates to LEFT-TOP
     int overlap(int[][] img1, int[][] img2, int i1,int j1, int i2, int j2){
         int count = 0;
-        for(int i=i1,x=i2; i>=0 && x>=0; i--,x--)
-            for(int j=j1, y=j2; j >=0 && y>=0 ; j--,y--)
-                if(img1[i][j] == img2[x][y])
-                    if(img1[i][j]==1)
-                        count++;
+        for(int i = i1, x = i2; i >= 0 && x >= 0; i--, x--)
+            for(int j = j1, y = j2; j >= 0 && y >= 0; j--, y--)
+                if(img1[i][j] == img2[x][y] && img1[i][j] == 1)
+                    count++;
         return count;
     }
 }
