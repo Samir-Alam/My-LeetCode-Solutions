aebebe30f056e2d6d69d36d25252fe06e7207b88
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-28 15:46:03 +0530

    Time: 10 ms (88.14%), Space: 46.1 MB (86.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,38 +1,8 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // List<List<String>> ans = new ArrayList<>();
-        // Map<String, List<String>> map = new HashMap<>();
-        // int n = strs.length;
-        // for(String s : strs){
-        //     List<String> temp = map.getOrDefault(sort(s), new ArrayList<>());
-        //     temp.add(s);
-        //     map.put(sort(s), temp);
-        // }
-        // for(String s : map.keySet())
-        //     ans.add(map.get(s));
-        // return ans;
-        
-        //2nd Solution
-        // if (strs == null || strs.length == 0) return new ArrayList<>();
-        // Map<String, List<String>> map = new HashMap<>();
-        // for (String s : strs) {
-        //     char[] ca = s.toCharArray();
-        //     Arrays.sort(ca);
-        //     String keyStr = String.valueOf(ca);
-        //     if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
-        //     map.get(keyStr).add(s);
-        // }
-        // return new ArrayList<>(map.values());
-        
-        //3rd Solution
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            // char[] ca = new char[26];
-            // for (char c : s.toCharArray()) 
-            //     ca[c - 'a']++;
-            // String keyStr = String.valueOf(ca);
-            if (!map.containsKey(sort(s))) 
-                map.put(sort(s), new ArrayList<>());
+            map.putIfAbsent(sort(s), new ArrayList<>());
             map.get(sort(s)).add(s);
         }
         return new ArrayList<>(map.values());
