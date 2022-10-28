0ace46e5b2d62e5cbcc52cbcbaac08d4361034ee
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-28 15:20:45 +0530

    Time: 10 ms (88.14%), Space: 45.9 MB (87.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,20 +3,19 @@ class Solution {
         List<List<String>> ans = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         int n = strs.length;
-        if(n == 1){
-            List<String> temp = new ArrayList<>();
-            temp.add(strs[0]);
-            ans.add(temp);
-            return ans;
-        }
+        // if(n == 1){
+        //     List<String> temp = new ArrayList<>();
+        //     temp.add(strs[0]);
+        //     ans.add(temp);
+        //     return ans;
+        // }
         for(String s : strs){
             List<String> temp = map.getOrDefault(sort(s), new ArrayList<>());
             temp.add(s);
             map.put(sort(s), temp);
         }
-        for(String s : map.keySet()){
+        for(String s : map.keySet())
             ans.add(map.get(s));
-        }
         return ans;
     }
     public String sort(String s){
