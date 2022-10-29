a263759a66d0c2078c03841a0322d02c0ed5f7ab
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-29 16:43:22 +0530

    Time: 3 ms (54.81%), Space: 43.2 MB (75.26%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -11,28 +11,42 @@ class Solution {
         // }
         // return ans;
         
-        List<String> combinations = new ArrayList();
-        generateAll(new char[2 * n], 0, combinations);
-        return combinations;
+    //     List<String> combinations = new ArrayList();
+    //     generateAll(new char[2 * n], 0, combinations);
+    //     return combinations;
+    // }
+    // public void generateAll(char[] current, int pos, List<String> result) {
+    //     if (pos == current.length) {
+    //         if (valid(current))
+    //             result.add(new String(current));
+    //     } else {
+    //         current[pos] = '(';
+    //         generateAll(current, pos+1, result);
+    //         current[pos] = ')';
+    //         generateAll(current, pos+1, result);
+    //     }
+    // }
+    // public boolean valid(char[] current) {
+    //     int balance = 0;
+    //     for (char c: current) {
+    //         if (c == '(') balance++;
+    //         else balance--;
+    //         if (balance < 0) return false;
+    //     }
+    //     return (balance == 0);
+        
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
     }
-    public void generateAll(char[] current, int pos, List<String> result) {
-        if (pos == current.length) {
-            if (valid(current))
-                result.add(new String(current));
-        } else {
-            current[pos] = '(';
-            generateAll(current, pos+1, result);
-            current[pos] = ')';
-            generateAll(current, pos+1, result);
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        if(str.length() == max*2){
+            list.add(str);
+            return;
         }
-    }
-    public boolean valid(char[] current) {
-        int balance = 0;
-        for (char c: current) {
-            if (c == '(') balance++;
-            else balance--;
-            if (balance < 0) return false;
-        }
-        return (balance == 0);
+        if(open < max)
+            backtrack(list, str+"(", open + 1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close + 1, max);
     }
 }
