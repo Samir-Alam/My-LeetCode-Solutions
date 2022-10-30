7ab458dbd3953c8cfe11fe1933ad1ef562b383bc
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-30 09:44:06 +0530

    Time: 240 ms (66.67%), Space: 263.6 MB (16.67%) - LeetHub

--- 2456-most-popular-video-creator/2456-most-popular-video-creator.java
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.java
@@ -1,55 +1,34 @@
 class Solution {
-    class Pair {
-        String id;
-        int views;
-        Pair(String i, int v) {
-            this.id = i;
-            this.views = v;
-        }
-    }
-    
-    class Custom_comparator implements Comparator<Pair>{
-
-        @Override
-        public int compare(Pair p1, Pair p2) {
-            if (p1.views == p2.views) return p1.id.compareTo(p2.id);
-            return p2.views-p1.views;
-        }
-    }
-    
     public List<List<String>> mostPopularCreator(String[] creators, String[] ids, int[] views) {
-        Map<String, Integer> viewsMap = new HashMap<>();    // [creator, total_views]
-        Map<String, List<Pair>> map = new HashMap<>();      // [creator, [(id, view)]]
-        
-        int n = ids.length;
-        for (int i = 0; i < n; i++) {
-            String creator = creators[i];
-            viewsMap.put(creator, viewsMap.getOrDefault(creator, 0) + views[i]);   // calculate total views for a creator
-            
-            if (!map.containsKey(creator)) {
-                map.put(creator, new ArrayList<Pair>()); 
-            }
-            map.get(creator).add(new Pair(ids[i], views[i]));                              // keep track of creator with [id, views]
-        }
-        
-        int maxViews = (Collections.max(viewsMap.values()));                  // get max views
-        List<String> popularCreators = new ArrayList<>();                      // stores creators with max views
-        for (var entry : viewsMap.entrySet()) {
-            if (entry.getValue()==maxViews) {
-                popularCreators.add(entry.getKey());  
-            }
-        }
-        
-        List<List<String>> ans = new ArrayList<>();
-        for (String creator : popularCreators) {
-            List<Pair> list = map.get(creator);
-            Collections.sort(list, new Custom_comparator());           // sort by views and if views are same for 2 ids, sort them lexicographically
-            List<String> ansList = new ArrayList<>();
-            ansList.add(creator);
-            ansList.add(list.get(0).id);                                        // popular creator with movie id having highest views
-            ans.add(ansList);
-        }
-        
-        return ans;
+        List<List<String>> res = new ArrayList();
+    	HashMap<String, Long> total = new HashMap();
+    	HashMap<String, Integer> highest = new HashMap();
+    	HashMap<String, String> id = new HashMap();
+    	long max=0;
+    	for(int i=0; i<creators.length; i++){
+    		long t = total.getOrDefault(creators[i], 0l);
+    		t=t+views[i];
+    		total.put(creators[i], t);
+    		int h = highest.getOrDefault(creators[i], -1);
+    		if(h==views[i]){
+    			String exis = id.get(creators[i]);
+    			if(exis.compareTo(ids[i])>0)
+    				id.put(creators[i], ids[i]);
+    		}
+    		if(h<views[i]){
+    			highest.put(creators[i], views[i]);
+    			id.put(creators[i], ids[i]);
+    		}
+    		max = Math.max(t, max);
+    	}
+    	for(Map.Entry<String, Long> e: total.entrySet()){
+    		if(e.getValue()==max){
+    			List<String> one = new ArrayList<String>();
+    			one.add(e.getKey());
+    			one.add(id.get(e.getKey()));
+    			res.add(one);
+    		}
+    	}
+    	return res;
     }
 }
