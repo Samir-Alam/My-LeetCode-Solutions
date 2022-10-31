fd38c56266554be7f11ab94f3acc4c5486ec30bf
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-10-31 11:32:02 +0530

    Time: 2 ms (42.33%), Space: 42.6 MB (86.02%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,24 +1,36 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int row = matrix.length, col = matrix[0].length;
-        for(int i = 0; i < row; i++){
-            int r = i, c = 0;
-            int ele = matrix[i][0];
-            while(r < row && c < col){
-                if(matrix[r][c] != ele)
+        // int row = matrix.length, col = matrix[0].length;
+        // for(int i = 0; i < row; i++){
+        //     int r = i, c = 0;
+        //     int ele = matrix[i][0];
+        //     while(r < row && c < col){
+        //         if(matrix[r][c] != ele)
+        //             return false;
+        //         r++;
+        //         c++;
+        //     }
+        // }
+        // for(int j = 0; j < col; j++){
+        //     int r = 0, c = j;
+        //     int ele = matrix[0][j];
+        //     while(r < row && c < col){
+        //         if(matrix[r][c] != ele)
+        //             return false;
+        //         r++;
+        //         c++;
+        //     }
+        // }
+        // return true;
+        
+        //2nd Solution
+        Map<Integer, Integer> groups = new HashMap();
+        for (int r = 0; r < matrix.length; ++r) {
+            for (int c = 0; c < matrix[0].length; ++c) {
+                if (!groups.containsKey(r-c))
+                    groups.put(r-c, matrix[r][c]);
+                else if (groups.get(r-c) != matrix[r][c])
                     return false;
-                r++;
-                c++;
-            }
-        }
-        for(int j = 0; j < col; j++){
-            int r = 0, c = j;
-            int ele = matrix[0][j];
-            while(r < row && c < col){
-                if(matrix[r][c] != ele)
-                    return false;
-                r++;
-                c++;
             }
         }
         return true;
