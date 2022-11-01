8b3bbeeb2c58129d35b52650416c4cef186d0ae2
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-01 14:37:10 +0530

    Time: 3 ms (58.35%), Space: 54.4 MB (33.23%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,18 +1,32 @@
 class Solution {
     public int[] findBall(int[][] grid) {
-        int result[] = new int[grid[0].length];
-        for (int i = 0; i < grid[0].length; i++)
-            result[i] = findBallDropColumn(0, i, grid);
-        return result;
+        // int result[] = new int[grid[0].length];
+        // for (int i = 0; i < grid[0].length; i++)
+        //     result[i] = findBallDropColumn(0, i, grid);
+        // return result;
+        int m = grid.length, n = grid[0].length, res[] = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int i1 = i, i2;
+            for (int j = 0; j < m; ++j) {
+                i2 = i1 + grid[j][i1];
+                if (i2 < 0 || i2 >= n || grid[j][i2] != grid[j][i1]) {
+                    i1 = -1;
+                    break;
+                }
+                i1 = i2;
+            }
+            res[i] = i1;
+        }
+        return res;
     }
-    public int findBallDropColumn(int row, int col, int[][] grid) {
-        if (row == grid.length)
-            return col;
-        int nextColumn = col + grid[row][col];
-        if (nextColumn < 0 ||
-                nextColumn > grid[0].length - 1 ||
-                grid[row][col] != grid[row][nextColumn])
-            return -1;
-        return findBallDropColumn(row + 1, nextColumn, grid);
-    }
+    // public int findBallDropColumn(int row, int col, int[][] grid) {
+    //     if (row == grid.length)
+    //         return col;
+    //     int nextColumn = col + grid[row][col];
+    //     if (nextColumn < 0 ||
+    //             nextColumn > grid[0].length - 1 ||
+    //             grid[row][col] != grid[row][nextColumn])
+    //         return -1;
+    //     return findBallDropColumn(row + 1, nextColumn, grid);
+    // }
 }
