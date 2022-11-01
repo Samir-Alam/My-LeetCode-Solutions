6f714b3e5b78a0d08ba528c912138e6d0c4dd9d6
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-01 14:38:56 +0530

    Time: 14 ms (20.85%), Space: 52.5 MB (84.07%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -4,20 +4,43 @@ class Solution {
         // for (int i = 0; i < grid[0].length; i++)
         //     result[i] = findBallDropColumn(0, i, grid);
         // return result;
-        int m = grid.length, n = grid[0].length, res[] = new int[n];
-        for (int i = 0; i < n; ++i) {
-            int i1 = i, i2;
-            for (int j = 0; j < m; ++j) {
-                i2 = i1 + grid[j][i1];
-                if (i2 < 0 || i2 >= n || grid[j][i2] != grid[j][i1]) {
-                    i1 = -1;
-                    break;
+        
+        //2nd Solution
+        // int m = grid.length, n = grid[0].length, res[] = new int[n];
+        // for (int i = 0; i < n; ++i) {
+        //     int i1 = i, i2;
+        //     for (int j = 0; j < m; ++j) {
+        //         i2 = i1 + grid[j][i1];
+        //         if (i2 < 0 || i2 >= n || grid[j][i2] != grid[j][i1]) {
+        //             i1 = -1;
+        //             break;
+        //         }
+        //         i1 = i2;
+        //     }
+        //     res[i] = i1;
+        // }
+        // return res;
+        
+        //DP solution
+        int result[] = new int[grid[0].length];
+        Integer memo[][] = new Integer[grid.length + 1][grid[0].length];
+        for (int row = grid.length; row >= 0; row--)
+            for (int col = 0; col < grid[0].length; col++) {
+                if (row == grid.length) {
+                    memo[row][col] = col;
+                    continue;
                 }
-                i1 = i2;
+                int nextColumn = col + grid[row][col];
+                if (nextColumn < 0 ||
+                        nextColumn > grid[0].length - 1 ||
+                        grid[row][col] != grid[row][nextColumn])
+                    memo[row][col] = -1;
+                else
+                    memo[row][col] = memo[row + 1][nextColumn];
+                if (row == 0)
+                    result[col] = memo[row][col];
             }
-            res[i] = i1;
-        }
-        return res;
+        return result;
     }
     // public int findBallDropColumn(int row, int col, int[][] grid) {
     //     if (row == grid.length)
