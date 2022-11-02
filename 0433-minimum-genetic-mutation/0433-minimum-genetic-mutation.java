f7e351a8afd7a56e4c149e5fd31119a79cdc3f65
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-02 07:39:21 +0530

    Time: 15 ms (11.41%), Space: 42.7 MB (10.70%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minMutation(String start, String end, String[] bank) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> seen = new HashSet<>();
+        queue.add(start);
+        seen.add(start);
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            int nodesInQueue = queue.size();
+            for (int j = 0; j < nodesInQueue; j++) {
+                String node = queue.remove();
+                if (node.equals(end))
+                    return steps;
+                for (char c: new char[] {'A', 'C', 'G', 'T'})
+                    for (int i = 0; i < node.length(); i++) {
+                        String neighbor = node.substring(0, i) + c + node.substring(i + 1);
+                        if (!seen.contains(neighbor) && Arrays.asList(bank).contains(neighbor)) {
+                            queue.add(neighbor);
+                            seen.add(neighbor);
+                        }
+                    }
+            }   
+            steps++;
+        }
+        return -1;
+    }
+}
