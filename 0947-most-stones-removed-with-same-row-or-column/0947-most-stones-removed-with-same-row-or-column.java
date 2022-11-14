8e2ae073ddc751d4088905916a5c927731d201fa
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-14 21:46:44 +0530

    Time: 506 ms (5.17%), Space: 49.7 MB (61.18%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,46 +1,69 @@
 class Solution {
-    public int removeStones(int[][] stones) {
-        if (stones == null || stones.length <= 1)
-            return 0;
+//     public int removeStones(int[][] stones) {
+//         if (stones == null || stones.length <= 1)
+//             return 0;
 
-        int n = stones.length;
-        UnionFind uf = new UnionFind();
+//         int n = stones.length;
+//         UnionFind uf = new UnionFind();
    
-        for (int[] edge : stones)
-            uf.union(edge[0] + 10001, edge[1]);
-        return n - uf.getCount();
-    }
+//         for (int[] edge : stones)
+//             uf.union(edge[0] + 10001, edge[1]);
+//         return n - uf.getCount();
+//     }
 
-    class UnionFind {
-        Map<Integer, Integer> parents;
-        int count;
+//     class UnionFind {
+//         Map<Integer, Integer> parents;
+//         int count;
     
-        public UnionFind() {
-            parents = new HashMap<>();
-            count = 0;
-        }
+//         public UnionFind() {
+//             parents = new HashMap<>();
+//             count = 0;
+//         }
     
-        public int getCount() {
-            return count;
-        }
+//         public int getCount() {
+//             return count;
+//         }
     
-        public int find(int x) {
-            if (!parents.containsKey(x)) {
-                parents.put(x, x);
-                count++;
+//         public int find(int x) {
+//             if (!parents.containsKey(x)) {
+//                 parents.put(x, x);
+//                 count++;
+//             }
+//             if (x != parents.get(x))
+//                 parents.put(x, find(parents.get(x)));
+//             return parents.get(x);
+//         }
+    
+//         public void union(int x, int y) {
+//             int rootX = find(x);
+//             int rootY = find(y);
+//             if (rootX == rootY)
+//                 return;
+//             parents.put(rootX, rootY);
+//             count--;
+//         }
+//     }
+    
+    public int removeStones(int[][] stones) {
+        Set<int[]> visited = new HashSet();
+        int numOfIslands = 0;
+        for (int[] s1:stones){
+            if (!visited.contains(s1)){
+               dfs(s1, visited, stones); 
+               numOfIslands++;
             }
-            if (x != parents.get(x))
-                parents.put(x, find(parents.get(x)));
-            return parents.get(x);
         }
+        return stones.length - numOfIslands;
+    }
     
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY)
-                return;
-            parents.put(rootX, rootY);
-            count--;
+    private void dfs(int[] s1, Set<int[]> visited, int[][] stones){
+        visited.add(s1);
+        for (int[] s2: stones){
+            if (!visited.contains(s2)){
+				// stone with same row or column. group them into island
+                if (s1[0] == s2[0] || s1[1] == s2[1])
+                    dfs(s2, visited, stones);
+            }
         }
     }
 }
