a232a96c177361517dd939b71576e8e659b4d72c
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-15 09:53:17 +0530

    Time: 0 ms (100.00%), Space: 49.9 MB (49.51%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,17 +16,17 @@
 class Solution {
     int count = 0;
     public int countNodes(TreeNode root) {
-        if(root == null)
-            return 0;
-        dfs(root);
-        return count;
+        // if(root == null)    return 0;
+        // dfs(root);
+        // return count;
+        
+        if(root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
     public void dfs(TreeNode root){
         if(root == null)    return;
         dfs(root.left);
-        if(root != null)
-            count++;
+        if(root != null)    count++;
         dfs(root.right);
-        
     }
 }
