a061a208293cff3a7170c2d537bbb952e3659dd5
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-18 09:06:08 +0530

    Time: 2 ms (80.60%), Space: 41.5 MB (34.70%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isUgly(int num) {
+        for (int i=2; i<6 && num>0; i++)
+            while (num % i == 0)
+                num /= i;
+        return num == 1;
+    }
+}
