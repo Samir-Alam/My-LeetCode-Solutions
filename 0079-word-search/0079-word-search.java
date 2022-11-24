e3f6c04117c2104ab2064c48c3832dc9b1e7e562
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-24 11:04:34 +0530

    Time: 203 ms (40.92%), Space: 41.8 MB (73.99%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,24 +1,36 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        char[] w = word.toCharArray();
-        for (int y=0; y<board.length; y++) {
-            for (int x=0; x<board[y].length; x++) {
-                if (exist(board, y, x, w, 0)) return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean exist(char[][] board, int y, int x, char[] word, int i) {
-        if (i == word.length) return true;
-        if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
-        if (board[y][x] != word[i]) return false;
-        board[y][x] ^= 256;
-        boolean exist = exist(board, y, x+1, word, i+1)
-            || exist(board, y, x-1, word, i+1)
-            || exist(board, y+1, x, word, i+1)
-            || exist(board, y-1, x, word, i+1);
-        board[y][x] ^= 256;
-        return exist;
-    }
+		int rows = board.length, cols = board[0].length;                                
+		if(word.length() == 0)
+            return true;                                         
+		if(rows * cols < word.length())
+            return false;                               
+		for(int r = 0; r < rows; r++)
+			for(int c = 0; c < cols; c++)
+				if(board[r][c] == word.charAt(0)){                                                
+					boolean isFound = dfsBoard(board, new boolean[rows][cols], r, c, word, 0);    
+					if(isFound)
+                        return true;
+				}
+		return false;                                                                             
+	}
+	public boolean dfsBoard(char[][] board, boolean[][] visitedBoard, int row, int col, String word, int wordInd) {        			
+		if((row<0 || row >= board.length) || (col < 0 || col >= board[0].length) || board[row][col] != word.charAt(wordInd))
+			return false;                                                                             
+		if(visitedBoard[row][col])
+            return false;
+		if(wordInd == word.length() - 1)
+            return true;
+		visitedBoard[row][col] = true;  
+		if( dfsBoard(board, visitedBoard, row, col - 1, word, wordInd + 1))
+            return true;
+		if( dfsBoard(board, visitedBoard, row, col+1, word, wordInd + 1))
+            return true; 
+		if( dfsBoard(board, visitedBoard, row-1, col, word, wordInd + 1))
+            return true;
+		if( dfsBoard(board, visitedBoard, row+1, col, word, wordInd + 1))
+            return true;
+		visitedBoard[row][col] = false;
+		return false;
+	}
 }
