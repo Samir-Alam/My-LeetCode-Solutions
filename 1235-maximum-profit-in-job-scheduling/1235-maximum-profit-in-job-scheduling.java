c02642ff49f13c8cd139861ff68e8bba1c65fa66
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-11-26 11:02:55 +0530

    Time: 49 ms (78.66%), Space: 50.5 MB (95.08%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+        }
+        Arrays.sort(jobs, (a, b)->a[1] - b[1]);
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        dp.put(0, 0);
+        for (int[] job : jobs) {
+            int cur = dp.floorEntry(job[0]).getValue() + job[2];
+            if (cur > dp.lastEntry().getValue())
+                dp.put(job[1], cur);
+        }
+        return dp.lastEntry().getValue();
+    }
+}
