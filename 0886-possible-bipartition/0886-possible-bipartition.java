bb704234cea87f34b9094e6a9482c8ea94c5c9ae
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-12-21 10:09:48 +0530

    Time: 28 ms (80.79%), Space: 50 MB (88.68%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -26,39 +26,80 @@
 //     }
 // }
 
-class UnionFind {
-    int[] parent;
-    int[] rank;
+// class UnionFind {
+//     int[] parent;
+//     int[] rank;
+//     public UnionFind(int size) {
+//         parent = new int[size];
+//         for (int i = 0; i < size; i++)
+//             parent[i] = i;
+//         rank = new int[size];
+//     }
+//     public int find(int x) {
+//         if (parent[x] != x)
+//             parent[x] = find(parent[x]);
+//         return parent[x];
+//     }
+//     public void union(int x, int y) {
+//         int xset = find(x), yset = find(y);
+//         if (xset == yset)
+//             return;
+//         else if (rank[xset] < rank[yset])
+//             parent[xset] = yset;
+//         else if(rank[xset] > rank[yset])
+//             parent[yset] = xset;
+//         else {
+//             parent[yset] = xset;
+//             rank[xset]++;
+//         }
+//     }
+// }
+// class Solution {
+//     public boolean possibleBipartition(int n, int[][] dislikes) {
+//         Map<Integer, List<Integer>> adj = new HashMap<>();
+//         for (int[] edge : dislikes) {
+//             int a = edge[0], b = edge[1];
+//             adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
+//             adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+//         }
+//         UnionFind dsu = new UnionFind(n + 1);
+//         for (int node = 1; node <= n; node++) {
+//             if (!adj.containsKey(node))
+//                 continue;
+//             for (int neighbor : adj.get(node)) {
+//                 if (dsu.find(node) == dsu.find(neighbor))
+//                     return false;
+//                 dsu.union(adj.get(node).get(0), neighbor);
+//             }
+//         }
+//         return true;
+//     }
+// }
 
-    public UnionFind(int size) {
-        parent = new int[size];
-        for (int i = 0; i < size; i++)
-            parent[i] = i;
-        rank = new int[size];
-    }
 
-    public int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    public void union(int x, int y) {
-        int xset = find(x), yset = find(y);
-        if (xset == yset) {
-            return;
-        } else if (rank[xset] < rank[yset]) {
-            parent[xset] = yset;
-        } else if (rank[xset] > rank[yset]) {
-            parent[yset] = xset;
-        } else {
-            parent[yset] = xset;
-            rank[xset]++;
-        }
-    }
-}
-
+//BFS
 class Solution {
+    public boolean bfs(int source, Map<Integer, List<Integer>> adj, int[] color) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(source);
+        color[source] = 0; // Start with marking source as `RED`.
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            if (!adj.containsKey(node))
+                continue;
+            for (int neighbor : adj.get(node)) {
+                if (color[neighbor] == color[node])
+                    return false;
+                if (color[neighbor] == -1) {
+                    color[neighbor] = 1 - color[node];
+                    q.add(neighbor);
+                }
+            }
+        }
+        return true;
+    }
+
     public boolean possibleBipartition(int n, int[][] dislikes) {
         Map<Integer, List<Integer>> adj = new HashMap<>();
         for (int[] edge : dislikes) {
@@ -67,16 +108,15 @@ class Solution {
             adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
         }
 
-        UnionFind dsu = new UnionFind(n + 1);
-        for (int node = 1; node <= n; node++) {
-            if (!adj.containsKey(node))
-                continue;
-            for (int neighbor : adj.get(node)) {
-                // Check if the node and its neighbor is in the same set.
-                if (dsu.find(node) == dsu.find(neighbor))
+        int[] color = new int[n + 1];
+        Arrays.fill(color, -1); // 0 stands for red and 1 stands for blue.
+
+        for (int i = 1; i <= n; i++) {
+            if (color[i] == -1) {
+                // For each pending component, run BFS.
+                if (!bfs(i, adj, color))
+                    // Return false, if there is conflict in the component.
                     return false;
-                // Move all the neighbours into same set as the first neighbour.
-                dsu.union(adj.get(node).get(0), neighbor);
             }
         }
         return true;
