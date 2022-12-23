88cb333fdb170ee7ca6a8dcf08dca158b2571f9b
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-12-23 09:58:50 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (92.35%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,15 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length <= 1) return 0;
+//         if(prices == null || prices.length <= 1) return 0;
 
-        int b0 = -prices[0], b1 = b0;
-        int s0 = 0, s1 = 0, s2 = 0;
+//         int b0 = -prices[0], b1 = b0;
+//         int s0 = 0, s1 = 0, s2 = 0;
 
-        for(int i = 1; i < prices.length; i++) {
-            b0 = Math.max(b1, s2 - prices[i]);
-            s0 = Math.max(s1, b1 + prices[i]);
-            b1 = b0; s2 = s1; s1 = s0; 
+//         for(int i = 1; i < prices.length; i++) {
+//             b0 = Math.max(b1, s2 - prices[i]);
+//             s0 = Math.max(s1, b1 + prices[i]);
+//             b1 = b0; s2 = s1; s1 = s0; 
+//         }
+//         return s0;
+        
+         int profit1=0, profit2=0;   
+        for(int i=1; i<prices.length; i++){
+            int copy=profit1;
+            profit1=Math.max(profit1+prices[i]-prices[i-1], profit2);
+            profit2=Math.max(copy, profit2);
         }
-        return s0;
+        return Math.max(profit1, profit2);
     }
 }
