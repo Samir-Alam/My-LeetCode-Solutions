082b67621d197b35db120eb59cc1f7a3eaf8ba71
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-12-23 10:00:05 +0530

    Time: 1 ms (90.22%), Space: 42.3 MB (42.93%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -12,11 +12,11 @@ class Solution {
 //         }
 //         return s0;
         
-         int profit1=0, profit2=0;   
-        for(int i=1; i<prices.length; i++){
-            int copy=profit1;
-            profit1=Math.max(profit1+prices[i]-prices[i-1], profit2);
-            profit2=Math.max(copy, profit2);
+         int profit1 = 0, profit2 = 0;   
+        for(int i = 1; i < prices.length; i++){
+            int copy = profit1;
+            profit1 = Math.max(profit1 + prices[i] - prices[i - 1], profit2);
+            profit2 = Math.max(copy, profit2);
         }
         return Math.max(profit1, profit2);
     }
