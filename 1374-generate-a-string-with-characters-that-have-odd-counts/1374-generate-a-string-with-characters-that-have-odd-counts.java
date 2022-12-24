f187a5d6f66c4809fd116f2f358815ade5252d5b
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-12-24 14:59:38 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (96.34%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,14 +1,19 @@
 class Solution {
     public String generateTheString(int n) {
-        StringBuilder sb = new StringBuilder();
-        if(n % 2 == 1)
-            for(int i = 0; i < n; i++)
-                sb.append('a');
-        else{
-            for(int i = 0; i < n - 1; i++)
-                sb.append('a');
-            sb.append('b');
-        }
-        return sb.toString();
+        // StringBuilder sb = new StringBuilder();
+        // if(n % 2 == 1)
+        //     for(int i = 0; i < n; i++)
+        //         sb.append('a');
+        // else{
+        //     for(int i = 0; i < n - 1; i++)
+        //         sb.append('a');
+        //     sb.append('b');
+        // }
+        // return sb.toString();
+        
+        char c[] = new char[n];
+        Arrays.fill(c, 'a');
+        if(n%2 == 0)    c[c.length - 1] = 'b';
+        return new String(c);
     }
 }
