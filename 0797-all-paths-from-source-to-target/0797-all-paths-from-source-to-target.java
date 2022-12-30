2eacb0e1208bcf393282d0944b28c79f32b3f085
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-12-30 21:55:44 +0530

    Time: 7 ms (31.66%), Space: 43.9 MB (92.91%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,18 +1,46 @@
 class Solution {
+    // public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+    //     List<List<Integer>> list = new ArrayList<>();
+    //     ArrayList<Integer> temp = new ArrayList<>();
+    //     temp.add(0);
+    //     solve(list, temp, graph, 0);
+    //     return list;
+    // }
+    // private void solve(List<List<Integer>> list,ArrayList<Integer> temp,int[][] graph,int i){
+    //     if(i == graph.length - 1)
+    //         list.add(new ArrayList<Integer>(temp));
+    //     for(int x : graph[i]){
+    //         temp.add(x);
+    //         solve(list, temp, graph, x);
+    //         temp.remove(temp.size() - 1);
+    //     }
+    // }
+    
+    //Iterative Solution
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> list = new ArrayList<>();
-        ArrayList<Integer> temp = new ArrayList<>();
-        temp.add(0);
-        solve(list, temp, graph, 0);
-        return list;
-    }
-    private void solve(List<List<Integer>> list,ArrayList<Integer> temp,int[][] graph,int i){
-        if(i == graph.length - 1)
-            list.add(new ArrayList<Integer>(temp));
-        for(int x : graph[i]){
-            temp.add(x);
-            solve(list, temp, graph, x);
-            temp.remove(temp.size() - 1);
+        Integer end = graph.length - 1;
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        Deque<Integer> path  = new ArrayDeque<>();
+        stack.push(0);
+        while (!stack.isEmpty()) {
+            Integer current = stack.peek();
+            if (current == path.peekLast()) {
+                stack.pop(); 
+                path.removeLast();
+                continue;
+            }
+            path.offer(current);
+            if (current == end) {
+                result.add(new ArrayList(path));
+                path.removeLast();
+                stack.pop(); 
+                continue;
+            }
+            for (int neighbor : graph[current]) {
+                stack.push(neighbor);
+            }
         }
+        return result;
     }
 }
