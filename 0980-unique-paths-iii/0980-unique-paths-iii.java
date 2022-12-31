90737236106ab1eeddd648d813893cbd6752cab7
Samir <66169191+Samir-Alam@users.noreply.github.com>
2022-12-31 10:41:59 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (93.56%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+    int res = 0, empty = 1, sx, sy, ex, ey;
+    public int uniquePathsIII(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) empty++;
+                else if (grid[i][j] == 1) {
+                    sx = i;
+                    sy = j;
+                }
+            }
+        }
+        dfs(grid, sx, sy);
+        return res;
+    }
+    public void dfs(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] < 0)
+            return;
+        if (grid[x][y] == 2) {
+            if (empty == 0) res++;
+            return;
+        }
+        grid[x][y] = -2;
+        empty--;
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+        grid[x][y] = 0;
+        empty++;
+    }
+}
