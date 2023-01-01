c1e9d4a7a9bc311ee7cc460a4f760fd310267859
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-01 09:47:20 +0530

    Time: 14 ms (100.00%), Space: 43.7 MB (100.00%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.java
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.java
@@ -1,18 +1,47 @@
 class Solution {
+//     public int distinctPrimeFactors(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int count = 0;
+//         for(int n : nums){
+//             int i = 2;
+//             while(n > 1){
+//                 if(n % i == 0){
+//                     map.put(i, map.getOrDefault(i, 0) + 1);
+//                     n /= i;
+//                 }
+//                 else i++;
+//             }
+//         }
+//         return map.size();
+        
+//     }
+    
     public int distinctPrimeFactors(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for(int n : nums){
-            int i = 2;
-            while(n > 1){
-                if(n % i == 0){
-                    map.put(i, map.getOrDefault(i, 0) + 1);
-                    n /= i;
-                }
-                else i++;
+        int c=0;
+        int[] a = new int[1001];
+        for(int x:nums)
+        {
+            prime(x,a);
+        }
+        for(int x:a)
+        {
+            if(x==1)
+                c++;
+        }
+        return c;
+    }
+    
+    private void prime(int x,int[] a)
+    {
+        int root = (int)Math.sqrt(x);
+        for (int i=2; i<=root; i++) {
+            while (x%i == 0) {
+                x/=i;
+                a[i]=1;
             }
         }
-        return map.size();
-        
+
+        if(x>=2)
+            a[x]=1;
     }
 }
