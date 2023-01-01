f976efa9c56a53bdcef7e30eae0fe1928c7b0050
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-01 11:37:48 +0530

    Time: 1 ms (95.36%), Space: 40.4 MB (74.62%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (words.length != pattern.length())
+            return false;
+        Map index = new HashMap();
+        for (Integer i=0; i<words.length; ++i)
+            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+                return false;
+        return true;
+    }
+}
