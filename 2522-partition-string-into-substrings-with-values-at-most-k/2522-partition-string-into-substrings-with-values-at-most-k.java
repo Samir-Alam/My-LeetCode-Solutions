d6ff94178b4f6d323ad579b77c8b26505c6201ba
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-01 17:42:38 +0530

    Time: 41 ms (100.00%), Space: 43 MB (100.00%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.java
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.java
@@ -1,14 +1,25 @@
 class Solution {
     public int minimumPartition(String s, int k) {
-        int i = 0, c = 0, j = 0;
-        while(j < s.length()){
-            while(j < s.length() && Long.parseLong(s.substring(i, j + 1)) <= k)
-                j++;
-            c++;
-            if(i == j)
-                return -1;
-            i = j;
-        }
-        return c;
+        // int i = 0, c = 0, j = 0;
+        // while(j < s.length()){
+        //     while(j < s.length() && Long.parseLong(s.substring(i, j + 1)) <= k)
+        //         j++;
+        //     c++;
+        //     if(i == j)
+        //         return -1;
+        //     i = j;
+        // }
+        // return c;
+        
+        int left = 0, right = 1, len = s.length(), count = 0;
+        for(right = 1; right <= len; right++)
+            if(right - left > 9 || Integer.parseInt(s.substring(left, right)) > k){
+                if(Integer.parseInt(s.substring(right - 1, right)) > k) 
+                    return -1;
+                else
+                    left = right-1; count++;
+            }
+        if(left < right) count++;
+        return count;
     }
 }
