7453079e887879955355ce78775b2319c7088d3f
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-02 11:25:26 +0530

    Time: 1 ms (100.00%), Space: 40.8 MB (77.90%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,16 +1,21 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        int capital = 0, small = 0;
-        for(char c : word.toCharArray()){
-            if(Character.isUpperCase(c))
-                capital++;
-            else
-                small++;
-        }
-        if(capital == 0 || small == 0)
-            return true;
-        else if(capital == 1)
-           return Character.isUpperCase(word.charAt(0));
-        return false;
+        // int capital = 0, small = 0;
+        // for(char c : word.toCharArray()){
+        //     if(Character.isUpperCase(c))
+        //         capital++;
+        //     else
+        //         small++;
+        // }
+        // if(capital == 0 || small == 0)
+        //     return true;
+        // else if(capital == 1)
+        //    return Character.isUpperCase(word.charAt(0));
+        // return false;
+        
+        //2nd Solution
+        int cnt = 0;
+        for(char c: word.toCharArray()) if('Z' - c >= 0) cnt++;
+        return ((cnt==0 || cnt==word.length()) || (cnt==1 && 'Z' - word.charAt(0)>=0));
     }
 }
