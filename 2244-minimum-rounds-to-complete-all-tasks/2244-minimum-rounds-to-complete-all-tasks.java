e1d10edafed983f14b17342a63046350f4b518e0
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-04 21:09:37 +0530

    Time: 69 ms (51.65%), Space: 104.1 MB (42.09%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,13 +1,28 @@
 class Solution {
     public int minimumRounds(int[] tasks) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        for (int a : tasks)
-            count.put(a, count.getOrDefault(a, 0) + 1);
-        int res = 0;
-        for (int freq : count.values()) {
-            if (freq == 1) return -1;
-            res += (freq + 2) / 3;
+        // HashMap<Integer, Integer> count = new HashMap<>();
+        // for (int a : tasks)
+        //     count.put(a, count.getOrDefault(a, 0) + 1);
+        // int res = 0;
+        // for (int freq : count.values()) {
+        //     if (freq == 1) 
+        //         return -1;
+        //     res += (freq + 2) / 3;
+        // }
+        // return res;
+        
+        Map<Integer, Integer> freq = new HashMap();
+        for (int task : tasks)
+            freq.put(task, freq.getOrDefault(task, 0) + 1);
+        int minimumRounds = 0;
+        for (int count : freq.values()) {
+            if (count == 1)
+                return - 1;
+            if (count % 3 == 0)
+                minimumRounds += count / 3;
+            else
+                minimumRounds += count / 3 + 1;
         }
-        return res;
+        return minimumRounds;
     }
 }
