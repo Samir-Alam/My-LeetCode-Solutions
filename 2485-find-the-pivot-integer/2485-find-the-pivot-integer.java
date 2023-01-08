6334914c34a05897c0f1c81515ce1f044edbef6c
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-08 22:05:07 +0530

    Time: 1 ms (96.38%), Space: 39.6 MB (83.03%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,23 +1,36 @@
 class Solution {
     public int pivotInteger(int n) {
-        if(n == 1)
-            return 1;
-        int r = n, l = 1;
-        int lsum = l, rsum = r;
-        while(l < r){
-            if(lsum < rsum){
-                l++;
-                lsum += l;
-            }else if(rsum < lsum){
-                r--;
-                rsum += r;
-            }else{
-                l++;
-                lsum += l;
-                r--;
-                rsum += r;
-            }
+        // if(n == 1)
+        //     return 1;
+        // int r = n, l = 1;
+        // int lsum = l, rsum = r;
+        // while(l < r){
+        //     if(lsum < rsum){
+        //         l++;
+        //         lsum += l;
+        //     }else if(rsum < lsum){
+        //         r--;
+        //         rsum += r;
+        //     }else{
+        //         l++;
+        //         lsum += l;
+        //         r--;
+        //         rsum += r;
+        //     }
+        // }
+        // return rsum == lsum ? l : -1;
+        
+        int sum = n * (n + 1) / 2;
+        int lo = 1, hi = n + 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int leftSum = (mid - 1) * mid / 2;
+            int rightSum = sum - (mid + 1) * mid / 2;
+            if (leftSum < rightSum)
+                lo = mid + 1;
+            else
+                hi = mid;
         }
-        return rsum == lsum ? l : -1;
+        return (lo - 1) * lo / 2 == sum - lo * (lo + 1) / 2 ? lo : -1;
     }
 }
