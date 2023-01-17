01d5776d7d00f1ad82badc22c999fe80481629e2
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-17 08:57:40 +0530

    Time: 17 ms (51.19%), Space: 42.7 MB (84.41%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -3,13 +3,13 @@ class Solution {
         int m = 0;
         for (int i = 0; i < s.length(); ++i)
             if (s.charAt(i) == '0')
-                ++m;
+                m++;
         int ans = m;
         for (int i = 0; i < s.length(); ++i){
             if (s.charAt(i) == '0')
                 ans = Math.min(ans, --m);
          else
-                ++m;
+                m++;
         }
         return ans;
     }
