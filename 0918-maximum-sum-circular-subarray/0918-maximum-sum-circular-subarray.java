045ff6a2be2eaae43bc42b00db09852517efa082
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-18 22:19:21 +0530

    Time: 6 ms (54.32%), Space: 47.9 MB (57.69%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -1,15 +1,25 @@
 class Solution {
     public int maxSubarraySumCircular(int[] A) {
-        int nonCircularSum = kadaneMaxSum(A);
-        int totalSum = 0;
-        for(int i = 0; i < A.length; i++){
-            totalSum += A[i];
-            A[i] = -A[i];
+        // int nonCircularSum = kadaneMaxSum(A);
+        // int totalSum = 0;
+        // for(int i = 0; i < A.length; i++){
+        //     totalSum += A[i];
+        //     A[i] = -A[i];
+        // }
+        // int circularSum = totalSum + kadaneMaxSum(A);
+        // if(circularSum == 0)
+        //     return nonCircularSum;
+        // return Math.max(circularSum, nonCircularSum);
+        
+        int curMax = 0, curMin = 0, sum = 0, maxSum = A[0], minSum = A[0];
+        for (int num : A) {
+            curMax = Math.max(curMax, 0) + num;
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin, 0) + num;
+            minSum = Math.min(minSum, curMin);
+            sum += num;  
         }
-        int circularSum = totalSum + kadaneMaxSum(A);
-        if(circularSum == 0)
-            return nonCircularSum;
-        return Math.max(circularSum, nonCircularSum);
+        return sum == minSum ? maxSum : Math.max(maxSum, sum - minSum);
     }
     int kadaneMaxSum(int[] A){
         int currentSum = A[0];
@@ -17,8 +27,8 @@ class Solution {
         for(int i = 1; i < A.length; i++){
             if(currentSum < 0)
                 currentSum = 0;
-            currentSum = A[i] + currentSum;
-            maxSum = Math.max(maxSum,currentSum);
+            currentSum += A[i];
+            maxSum = Math.max(maxSum, currentSum);
         }
         return maxSum;
     }
