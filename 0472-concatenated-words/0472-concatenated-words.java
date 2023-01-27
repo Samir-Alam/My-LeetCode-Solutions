2f436f6368fc0c435fdf463efc4851d95e224eff
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-27 10:15:16 +0530

    Time: 100 ms (39.21%), Space: 73 MB (23.35%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -2,31 +2,25 @@ class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> result = new ArrayList<>();
         Set<String> set = new HashSet<>();
-        for (String word : words) {
+        for (String word : words)
             set.add(word);
-        }
         for (String word : words) {
             set.remove(word);
-            if (canForm(word, set)) {
+            if (canForm(word, set))
                 result.add(word);
-            }
             set.add(word);
         }
         return result;
     }
 
     private static boolean canForm(String word, Set<String> set) {
-        if (word.length() == 0) {
+        if (word.length() == 0)
             return true;
-        }
         boolean[] dp = new boolean[word.length() + 1];
         dp[0] = true;
         for (int i = 1; i <= word.length(); i++) {
             for (int j = 0; j < i; j++) {
-                if (!dp[j]) {
-                    continue;
-                }
-                if (set.contains(word.substring(j, i))) {
+                if (dp[j] && set.contains(word.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
