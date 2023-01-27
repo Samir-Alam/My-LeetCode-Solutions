8c10e476cf64bb0fc25e72f55196604e5d9ddc05
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-01-27 10:15:56 +0530

    Time: 71 ms (62.70%), Space: 48.1 MB (65.64%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -18,14 +18,12 @@ class Solution {
             return true;
         boolean[] dp = new boolean[word.length() + 1];
         dp[0] = true;
-        for (int i = 1; i <= word.length(); i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i <= word.length(); i++)
+            for (int j = 0; j < i; j++)
                 if (dp[j] && set.contains(word.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
-            }
-        }
         return dp[word.length()];
     }
 }
