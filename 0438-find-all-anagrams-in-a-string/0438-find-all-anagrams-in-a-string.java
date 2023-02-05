035912ae0f30a4bff5f6853f41f75cb0aac2084a
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-02-05 12:04:18 +0530

    Time: 4 ms (99.42%), Space: 43.1 MB (92.34%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,33 +1,63 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int[] charsInP = new int[26];
-        calculateFreq(p, charsInP);
-        int len1, len2;
-        len1 = s.length();
-        len2 = p.length();
-        List<Integer> res = new ArrayList<>();
-        for(int i = 0; i + len2 <= len1; i++){
-            String subs = s.substring(i, i + len2);
-            int[] charsInSubs = new int[26];
-            calculateFreq(subs, charsInSubs);
-            if(equalFreq(charsInP, charsInSubs)){
-                res.add(i);
-            }
-        }
-        return res;
-    }
+//         int[] charsInP = new int[26];
+//         calculateFreq(p, charsInP);
+//         int len1, len2;
+//         len1 = s.length();
+//         len2 = p.length();
+//         List<Integer> res = new ArrayList<>();
+//         for(int i = 0; i + len2 <= len1; i++){
+//             String subs = s.substring(i, i + len2);
+//             int[] charsInSubs = new int[26];
+//             calculateFreq(subs, charsInSubs);
+//             if(equalFreq(charsInP, charsInSubs)){
+//                 res.add(i);
+//             }
+//         }
+//         return res;
+//     }
     
-    private void calculateFreq(String s, int[] chars){
-        for(char ch : s.toCharArray()){
-            chars[ch - 'a']++;
-        }
-    }
-    private boolean equalFreq(int[] chars1, int[] chars2){
-        for(int i = 0; i < 26; i++){
-            if(chars1[i] != chars2[i]){
-                return false;
+//     private void calculateFreq(String s, int[] chars){
+//         for(char ch : s.toCharArray()){
+//             chars[ch - 'a']++;
+//         }
+//     }
+//     private boolean equalFreq(int[] chars1, int[] chars2){
+//         for(int i = 0; i < 26; i++){
+//             if(chars1[i] != chars2[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        List<Integer> ans = new ArrayList<>();
+            int n = s.length();
+            int m = p.length();
+			if(m>n){
+                return ans;
             }
-        }
-        return true;
+			int []arrs = new int[26];
+			int []arrp = new int[26];
+			for(int i=0;i<m;i++){
+				arrs[s.charAt(i)-'a']++;
+				arrp[p.charAt(i)-'a']++;
+			}
+			for(int i=0;i<=n-m;i++){
+				boolean flag = true;
+				for(int j=0;j<26;j++){
+					if(arrs[j]!=arrp[j]){
+						flag = false;
+						break;
+					}
+				}
+				if(flag==true){
+                    ans.add(i);
+                }
+				arrs[s.charAt(i)-'a']--;
+				if(i+m < n){
+                    arrs[s.charAt(i+m)-'a']++;
+                }
+			}
+			return ans;
     }
 }
