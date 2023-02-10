58a8ccd0e9a5448e1170e41e18a7ce9122a1e876
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-02-10 18:39:36 +0530

    Time: 5 ms (100.00%), Space: 43.5 MB (60.36%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,25 +1,38 @@
 class Solution {
     public int maxDistance(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        final int MAX_DISTANCE = rows + cols + 1;
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 1)
-                    grid[i][j] = 0;
-                else {
-                    grid[i][j] = MAX_DISTANCE;
-                    grid[i][j] = Math.min(grid[i][j], Math.min(i > 0 ? grid[i - 1][j] + 1 : MAX_DISTANCE,
-                                                               j > 0 ? grid[i][j - 1] + 1 : MAX_DISTANCE));
+        int m = grid.length;
+        int n = grid[0].length;
+        int x= n+m;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    continue;
+                }  
+                int top=x;
+                int left=x;
+                if(i-1>=0) top=grid[i-1][j];
+                if(j-1>=0) left=grid[i][j-1];
+                grid[i][j]=Math.min(top,left)+1;
+            }
+        }   
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(grid[i][j]==1) {
+                    continue;
+                }
+                int bottem=x;
+                int right=x;
+                if(i+1<m) bottem=grid[i+1][j];
+                if(j+1<n) right=grid[i][j+1];
+                grid[i][j]=Math.min(grid[i][j],Math.min(bottem,right)+1);
+            }
+        }      
+        int count=Integer.MIN_VALUE;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                count=Math.max(count,grid[i][j]);
                 }
             }
-        int ans = Integer.MIN_VALUE;
-        for (int i = rows - 1; i >= 0; i--)
-            for (int j = cols - 1; j >= 0; j--) {
-                grid[i][j] = Math.min(grid[i][j], Math.min(i < rows - 1 ? grid[i + 1][j] + 1 : MAX_DISTANCE,
-                                                           j < cols - 1 ? grid[i][j + 1] + 1 : MAX_DISTANCE));
-                ans = Math.max(ans, grid[i][j]);
-            }
-        return ans == 0 || ans == MAX_DISTANCE ? -1 : ans;
+        return count-1==n+m+1||count-1==0 ? -1: count-1;
     }
-};
+}
