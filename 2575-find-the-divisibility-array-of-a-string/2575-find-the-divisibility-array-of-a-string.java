516c53834a489d7ddce0a32ccfa5562cca828124
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-02-26 10:25:27 +0530

    Time: 14 ms (25.00%), Space: 53 MB (25.00%) - LeetHub

--- 2575-find-the-divisibility-array-of-a-string/2575-find-the-divisibility-array-of-a-string.java
+++ 2575-find-the-divisibility-array-of-a-string/2575-find-the-divisibility-array-of-a-string.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] divisibilityArray(String word, int m) {
-        int l=word.length();
-         long digi=0;
-        int ans[]=new int[l];
-        int j=0;
-        for(int i=0;i<l;i++){
-            int cur=word.charAt(i)-'0';
-            digi=(digi*10+cur)%m;
-            if(digi==0){
-                ans[j++]=1;
-            }else{
-                ans[j++]=0;
-            }
+        int l = word.length();
+         long digi = 0;
+        int ans[] = new int[l];
+        int j = 0;
+        for(int i = 0; i < l; i++){
+            int cur = word.charAt(i) - '0';
+            digi = (digi * 10 + cur) % m;
+            if(digi == 0)
+                ans[j++] = 1;
+            else
+                ans[j++] = 0;
         }
         return ans;
     }
