32249d8a69395823c86d7f054b6b62d0281da533
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-03-08 15:29:56 +0530

    Time: 15 ms (91.99%), Space: 44.1 MB (18.45%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,30 @@
 class Solution {
-    public boolean ispos(int[] piles, int h,int k){
-        int time=0;
-        for(int i=0;i<piles.length;i++){
-            time+=piles[i]/k;
-            if(piles[i]%k>0)
-                time+=1;
+    public boolean ispos(int[] piles, int h, int k){
+        int time = 0;
+        for(int i = 0; i < piles.length; i++){
+            time += piles[i] / k;
+            if(piles[i] % k > 0)
+                time += 1;
             if(time > h)
                 return false;
         }
         if(h < time)return false;
         return true;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
         int maxi = -1;
-        for(int i:piles){
-            maxi = Math.max(i,maxi);
-        }
-        int l=1,r = maxi,mid;
+        for(int i : piles)
+            maxi = Math.max(i, maxi);
+        int l = 1, r = maxi, mid;
         int k = -1;
         while(l <= r){
-            mid = l + (r-l)/2;
-            if(ispos(piles,h,mid)){
-                k=mid;
-                r = mid-1;
+            mid = l + (r - l) / 2;
+            if(ispos(piles, h, mid)){
+                k = mid;
+                r = mid - 1;
             }
-            else{
-                l=mid+1;
-            }
+            else
+                l = mid + 1;
         }
         return k;
     }
