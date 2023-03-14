2834053587f413c916a6c70257667cb7548a51ce
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-03-14 19:15:48 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (28.58%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,22 +14,38 @@
  * }
  */
 class Solution {
-    int sum;
-    public void inorder(TreeNode root,String str){
-        if(root!=null && root.left == null && root.right == null){
-            str += Integer.toString(root.val);
-            sum += Integer.parseInt(str);
-            return;
-        }
-        else if(root != null){
-            inorder(root.left, str + Integer.toString(root.val));
-            inorder(root.right, str + Integer.toString(root.val));
-        }
-        
-    }
+    // int sum;
+    // public void inorder(TreeNode root,String str){
+    //     if(root != null && root.left == null && root.right == null){
+    //         str += Integer.toString(root.val);
+    //         sum += Integer.parseInt(str);
+    //         return;
+    //     }
+    //     else if(root != null){
+    //         inorder(root.left, str + Integer.toString(root.val));
+    //         inorder(root.right, str + Integer.toString(root.val));
+    //     }
+    // }
+    // public int sumNumbers(TreeNode root) {
+    //     sum = 0;
+    //     inorder(root, "");
+    //     return sum;
+    // }
+    
     public int sumNumbers(TreeNode root) {
-        sum = 0;
-        inorder(root, "");
-        return sum;
+       return findSum(root,0);
+    }
+    public int findSum(TreeNode root, int sum){
+        if(root == null)
+            return -1;
+        sum *= 10;
+        sum += root.val;
+        int leftSum = findSum(root.left, sum);
+        int rightSum = findSum(root.right, sum);
+        if(leftSum < 0 && rightSum < 0)
+            return sum;
+        leftSum = leftSum >= 0 ?  leftSum : 0;
+        rightSum = rightSum >= 0 ? rightSum : 0;
+        return leftSum+rightSum;
     }
 }
