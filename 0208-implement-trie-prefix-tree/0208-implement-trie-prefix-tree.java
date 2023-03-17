40a72b6e293b255c54778a42cb5cf4b2ee76aef0
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-03-17 14:46:20 +0530

    Time: 41 ms (49.79%), Space: 52 MB (58.93%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,17 +1,73 @@
 class Trie {
 
-    HashSet<String> hs;
+    class TrieNode {
+
+        // R links to node children
+        private TrieNode[] links;
+
+        private final int R = 26;
+
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch -'a'] != null;
+        }
+        public TrieNode get(char ch) {
+            return links[ch -'a'];
+        }
+        public void put(char ch, TrieNode node) {
+            links[ch -'a'] = node;
+        }
+        public void setEnd() {
+            isEnd = true;
+        }
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
+    // HashSet<String> hs;
+    private TrieNode root;
     public Trie() {
-        hs = new HashSet<>();
+        // hs = new HashSet<>();
+        root = new TrieNode();
     }
     public void insert(String word) {
-        hs.add(word);
+        // hs.add(word);
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+           char curLetter = word.charAt(i);
+           if (node.containsKey(curLetter)) {
+               node = node.get(curLetter);
+           } else {
+               return null;
+           }
+        }
+        return node;
     }
     public boolean search(String word) {
-       return hs.contains(word) ? true : false;
+       // return hs.contains(word) ? true : false;
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
     }
     public boolean startsWith(String prefix) {
-        return hs.stream().filter(s -> s.startsWith(prefix)).findAny().isPresent();
+        // return hs.stream().filter(s -> s.startsWith(prefix)).findAny().isPresent();
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
     }
 }
 
