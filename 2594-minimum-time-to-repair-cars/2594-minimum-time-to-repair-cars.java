1c406752f09a7b62f5ffe248e020660e8d82d69d
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-03-19 00:04:01 +0530

    Time: 119 ms (100.00%), Space: 50.7 MB (100.00%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.java
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.java
@@ -3,25 +3,19 @@ class Solution {
         long minTime = Integer.MAX_VALUE;
         long maxTime = 0;
         for(int i = 0; i < ranks.length; i++)
-        {
             minTime = Math.min(minTime, ranks[i]);
-        }
-        maxTime = minTime*cars*cars;
+        maxTime = minTime * cars * cars;
         long low = minTime;
         long high = maxTime;
         long minTimeTaken = minTime;
-        while(low <= high)
-        {
+        while(low <= high){
             long curTime = low + (high - low) / 2;
             long carsRepaired = 0;
             for(int i = 0; i < ranks.length; i++)
-            {
-                carsRepaired+=(int)Math.sqrt(curTime / ranks[i]);
-            }
+                carsRepaired += (int)Math.sqrt(curTime / ranks[i]);
             if(carsRepaired < cars)
             low = curTime + 1;
-            else
-            {
+            else{
                 minTimeTaken = curTime;
                 high = curTime - 1;
             }
