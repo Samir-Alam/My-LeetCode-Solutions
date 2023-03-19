9433773bde98129298341b19087109002cbe58f7
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-03-19 19:46:12 +0530

    Time: 889 ms (39.55%), Space: 113.6 MB (18.45%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,61 +1,43 @@
 class TrieNode {
     Map<Character, TrieNode> children;
-    boolean isWord;
-    
+    boolean isWord;   
     public TrieNode() {
         children = new HashMap<>();
         isWord = false;
     }
 }
-
 class WordDictionary {
     private TrieNode root;
-
-    public WordDictionary() {
+    public WordDictionary(){
         root = new TrieNode();
     }
-
     public void addWord(String word) {
         TrieNode node = root;
-    
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-
-            if (!node.children.containsKey(c)) {
-                node.children.put(c, new TrieNode());
-            }
-            
+            if (!node.children.containsKey(c))
+                node.children.put(c, new TrieNode());   
             node = node.children.get(c);
-        }
-        
+        }   
         node.isWord = true;
     }
-
-    public boolean search(String word) {
+    public boolean search(String word){
         return searchHelper(root, word, 0);
     }
-
     private boolean searchHelper(TrieNode node, String word, int index) {
-    
-        if (index == word.length()) {
+        if (index == word.length())
             return node.isWord;
-        }
         char c = word.charAt(index);
         if (c == '.') {
-            
-            for (TrieNode child : node.children.values()) {
-                if (searchHelper(child, word, index + 1)) {
+            for (TrieNode child : node.children.values())
+                if (searchHelper(child, word, index + 1))
                     return true;
-                }
-            }
-           
             return false;
         } 
         else {   
             TrieNode child = node.children.get(c);
-            if (child == null) {
+            if (child == null)
                 return false;
-            }
             return searchHelper(child, word, index + 1);
         }
     }
