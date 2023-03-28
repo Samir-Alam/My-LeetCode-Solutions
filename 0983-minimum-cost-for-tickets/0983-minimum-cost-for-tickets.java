008b21c49bb0ac2fad55c8ee16974e700967fc8d
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-03-28 17:45:09 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (39.24%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -7,7 +7,6 @@ class Solution {
     public int recursion(int[] days, int i, int[] costs) {
         if(i >= days.length)
             return 0;
-        
         if(dp[i] != null)
             return dp[i];
         // 1day
@@ -16,18 +15,16 @@ class Solution {
         int res2 = costs[1] + recursion(days, calculate(i,7,days), costs);
         // 1 month
         int res3 = costs[2] + recursion(days, calculate(i,30,days), costs);
-        
         return dp[i] = Math.min(Math.min(res1, res2), res3);
     }
     public int calculate(int i, int day, int[] days) {
         int lastDay = days[i] + day;
         int res = 366;
-        for(int j=i; j<days.length; j++) {
+        for(int j = i; j < days.length; j++)
             if(days[j] >= lastDay) {
                 res = j;
                 break;
             }
-        }
         return res;
     }
 }
