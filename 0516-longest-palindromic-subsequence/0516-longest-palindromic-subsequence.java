8a3a248c7c42def97c0017a4721867ea0677a063
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-04-14 10:05:06 +0530

    Time: 67 ms (51.10%), Space: 52 MB (35.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,10 +1,4 @@
 class Solution {
-    // public int countPalindrome(String s,int i,int j){
-    //     if(i>j) return 0;
-    //     if(i==j) return 1;
-    //     if(s.charAt(i)==s.charAt(j)) return 2+ countPalindrome(s,i+1,j-1);
-    //     return Math.max(countPalindrome(s,i+1,j),countPalindrome(s,i,j-1));
-    // }
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
         int dp[][] = new int[n+1][n+1];
@@ -16,7 +10,6 @@ class Solution {
                 else dp[i][j] = Math.max(dp[i + 1][j],dp[i][j - 1]);
             }
         }
-        // return countPalindrome(s,0,n-1);
         return dp[0][n - 1];
     }
 }
