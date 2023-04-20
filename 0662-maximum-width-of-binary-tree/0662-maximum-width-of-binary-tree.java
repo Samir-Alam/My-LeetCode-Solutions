11c07ae6d38b523fed6017807c5db8f0621da63c
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-04-20 19:30:31 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (28.18%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -15,26 +15,55 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-        int ans = 0;
-        Queue<pair> q = new LinkedList<>();
-        q.offer(new pair(root, 0));
-        while(!q.isEmpty()){
-            int size = q.size();
-            int min = q.peek().num;
-            int start = 0, end = 0;
-            for(int i = 0; i<size; i++){
-                int curNodeNum = q.peek().num-min;
-                TreeNode node = q.peek().node;
-                q.poll();
-                if(i==0) start = curNodeNum;
-                if(i == size-1) end = curNodeNum;
-                if(node.left != null) q.offer(new pair(node.left, curNodeNum*2+1));
-                if(node.right != null) q.offer(new pair(node.right, curNodeNum*2+2));
+        // if(root == null) return 0;
+        // int ans = 0;
+        // Queue<pair> q = new LinkedList<>();
+        // q.offer(new pair(root, 0));
+        // while(!q.isEmpty()){
+        //     int size = q.size();
+        //     int min = q.peek().num;
+        //     int start = 0, end = 0;
+        //     for(int i = 0; i < size; i++){
+        //         int curNodeNum = q.peek().num - min;
+        //         TreeNode node = q.peek().node;
+        //         q.poll();
+        //         if(i == 0) start = curNodeNum;
+        //         if(i == size - 1) end = curNodeNum;
+        //         if(node.left != null) q.offer(new pair(node.left, curNodeNum * 2 + 1));
+        //         if(node.right != null) q.offer(new pair(node.right, curNodeNum * 2 + 2));
+        //     }
+        //     ans = Math.max(ans, end-start+1);
+        // }
+        // return ans;
+        if (root == null) return 0;
+        
+        Deque<TreeNode> bfsQueue = new LinkedList<>();
+        
+        root.val = 0;
+        bfsQueue.addLast(root);
+        
+        int answer = 1;
+        
+        while (!bfsQueue.isEmpty()) {
+            int size = bfsQueue.size();
+            
+            answer = Math.max(answer, bfsQueue.getLast().val - bfsQueue.getFirst().val + 1);
+            while (size-- != 0) {
+                TreeNode cur = bfsQueue.removeFirst();
+                
+                if (cur.left != null) {
+                    cur.left.val = 2 * cur.val;
+                    bfsQueue.addLast(cur.left);
+                }
+                
+                if (cur.right != null) {
+                    cur.right.val = 2 * cur.val + 1;
+                    bfsQueue.addLast(cur.right);
+                }
             }
-            ans = Math.max(ans, end-start+1);
         }
-        return ans;
+        
+        return answer;
     }
 }
 class pair{
