fd9a93bccaa32a35ceca4457838c8f6992bae6de
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-04-20 14:12:18 +0530

    Time: 1 ms (100.00%), Space: 42.9 MB (9.09%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -20,19 +20,19 @@ class Solution {
         Queue<pair> q = new LinkedList<>();
         q.offer(new pair(root, 0));
         while(!q.isEmpty()){
-          int size = q.size();
-          int min = q.peek().num;
-          int start = 0, end = 0;
-          for(int i = 0; i<size; i++){
-            int curNodeNum = q.peek().num-min;
-            TreeNode node = q.peek().node;
-            q.poll();
-            if(i==0) start = curNodeNum;
-            if(i == size-1) end = curNodeNum;
-            if(node.left != null) q.offer(new pair(node.left, curNodeNum*2+1));
-            if(node.right != null) q.offer(new pair(node.right, curNodeNum*2+2));
-          }
-          ans = Math.max(ans, end-start+1);
+            int size = q.size();
+            int min = q.peek().num;
+            int start = 0, end = 0;
+            for(int i = 0; i<size; i++){
+                int curNodeNum = q.peek().num-min;
+                TreeNode node = q.peek().node;
+                q.poll();
+                if(i==0) start = curNodeNum;
+                if(i == size-1) end = curNodeNum;
+                if(node.left != null) q.offer(new pair(node.left, curNodeNum*2+1));
+                if(node.right != null) q.offer(new pair(node.right, curNodeNum*2+2));
+            }
+            ans = Math.max(ans, end-start+1);
         }
         return ans;
     }
