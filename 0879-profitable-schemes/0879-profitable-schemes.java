4dcad8bf574f53a2fd4fd70642aade5fc549da07
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-04-21 08:39:08 +0530

    Time: 123 ms (26.71%), Space: 53.1 MB (20.55%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,36 +1,20 @@
 class Solution {
     int mod = 1000000007;
     int[][][] memo = new int[101][101][101];
-    
     int find(int pos, int count, int profit, int n, int minProfit, int[] group, int[] profits) {
-        if (pos == group.length) {
-            // If profit exceeds the minimum required; it's a profitable scheme.
+        if (pos == group.length)
             return profit >= minProfit ? 1 : 0;
-        }
-        
-        if (memo[pos][count][profit] != -1) {
-            // Repeated subproblem, return the stored answer.
+        if (memo[pos][count][profit] != -1)
             return memo[pos][count][profit];
-        }
-        
-        // Ways to get a profitable scheme without this crime.
         int totalWays = find(pos + 1, count, profit, n, minProfit, group, profits);
-        if (count + group[pos] <= n) {
-            // Adding ways to get profitable schemes, including this crime.
+        if (count + group[pos] <= n)
             totalWays += find(pos + 1, count + group[pos], Math.min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
-        }
-        
         return memo[pos][count][profit] = totalWays % mod;
     }
-    
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
-        // Initializing all states as -1.
-        for (int i = 0; i <= group.length; i++) {
-            for(int j = 0; j <= n; j++) {
+        for (int i = 0; i <= group.length; i++)
+            for(int j = 0; j <= n; j++)
                 Arrays.fill(memo[i][j], -1);
-            }
-        }
-        
         return find(0, 0, 0, n, minProfit, group, profit);
     }
 }
