2dfa9953433a3eba0fab36aab3062c394c845602
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-05-04 08:31:33 +0530

    Time: 619 ms (5.11%), Space: 51.4 MB (6.82%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -14,33 +14,90 @@ class Solution {
         return loopAround;
     }
     public String predictPartyVictory(String senate) {
-        StringBuilder senateArray = new StringBuilder(senate);
-        int rCount = 0;
-        int dCount = 0;
-        for (int i = 0; i < senateArray.length(); i++) {
-            if (senateArray.charAt(i) == 'R')
-                rCount++;
-            else
-                dCount++;
-        }
-        int turn = 0;
-        while (rCount > 0 && dCount > 0) {
-            if (senateArray.charAt(turn) == 'R') {
-                boolean bannedSenatorBefore = ban(senateArray, 'D', (turn + 1) % senateArray.length());
-                if (bannedSenatorBefore)
-                    turn--;
-                dCount--;
-            } else {
-                boolean bannedSenatorBefore = ban(senateArray, 'R', (turn + 1) % senateArray.length());
-                if (bannedSenatorBefore)
-                    turn--;
-                rCount--;
+        // StringBuilder senateArray = new StringBuilder(senate);
+        // int rCount = 0;
+        // int dCount = 0;
+        // for (int i = 0; i < senateArray.length(); i++) {
+        //     if (senateArray.charAt(i) == 'R')
+        //         rCount++;
+        //     else
+        //         dCount++;
+        // }
+        // int turn = 0;
+        // while (rCount > 0 && dCount > 0) {
+        //     if (senateArray.charAt(turn) == 'R') {
+        //         boolean bannedSenatorBefore = ban(senateArray, 'D', (turn + 1) % senateArray.length());
+        //         if (bannedSenatorBefore)
+        //             turn--;
+        //         dCount--;
+        //     } else {
+        //         boolean bannedSenatorBefore = ban(senateArray, 'R', (turn + 1) % senateArray.length());
+        //         if (bannedSenatorBefore)
+        //             turn--;
+        //         rCount--;
+        //     }
+        //     turn = (turn + 1) % senateArray.length();
+        // }
+        // if (rCount > 0)
+        //     return "Radiant";
+        // else
+        //     return "Dire";
+        
+        int senateLen = senate.length();
+        boolean[] isBanned = new boolean[senateLen];
+
+        int noRadiant = 0;
+        int noDire = 0;
+
+        for(char c: senate.toCharArray()){
+            if(c == 'R'){
+                noRadiant ++;
             }
-            turn = (turn + 1) % senateArray.length();
+            if(c == 'D'){
+                noDire ++;
+            }
         }
-        if (rCount > 0)
-            return "Radiant";
-        else
+
+        return hasWon(noRadiant, noDire, 0, senate, isBanned);
+    }
+    public String hasWon(int noRadiant, int noDire, int index, String senate, boolean[] isBanned){
+        if(noRadiant == 0){
             return "Dire";
+        }
+        if(noDire == 0){
+            return "Radiant";
+        }
+        
+
+        if(isBanned[index] == true){
+            return hasWon(noRadiant, noDire, (index + 1) % senate.length(), senate, isBanned);
+        }
+
+        char currentCharacter = senate.charAt(index);
+        if(currentCharacter == 'R'){
+            for(int i = 1; i < senate.length(); i++){
+                int nextLoc = (index + i) % senate.length();
+                if(senate.charAt(nextLoc) == 'D' && isBanned[nextLoc] == false){
+                    noDire --;
+                    isBanned[nextLoc] = true;
+                    break;
+                }
+            }
+            return hasWon(noRadiant, noDire, (index+1) % senate.length(), senate, isBanned);
+        }
+
+        if(currentCharacter == 'D'){
+            for(int i = 1; i < senate.length(); i++){
+                int nextLoc = (index + i) % senate.length();
+                if(senate.charAt(nextLoc) == 'R' && isBanned[nextLoc] == false){
+                    noRadiant --;
+                    isBanned[nextLoc] = true;
+                    break;
+                }
+            }
+            return hasWon(noRadiant, noDire, (index+1) % senate.length(), senate, isBanned);
+        }
+        
+        return "";
     }
 }
