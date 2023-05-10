eb80da61d455927ba7520cc56416c4bb201d64e1
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-05-10 11:38:40 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (69.39%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -7,25 +7,18 @@ class Solution {
         int ec = n - 1;
         int a = 1;
         while(sr <= er && sc <= ec){
-            for(int i = sc; i <= ec; i++){
+            for(int i = sc; i <= ec; i++)
                 arr[sr][i] = a++;
-            }
-
-            for(int i = sr + 1; i <= er; i++){
+            for(int i = sr + 1; i <= er; i++)
                 arr[i][ec] = a++;
-            }
-
             for(int i = ec - 1; i >= sc; i--){
-                if(sr == er){
+                if(sr == er)
                     break;
-                }
                 arr[er][i] = a++;
             }
-
             for(int i = er - 1; i >= sr + 1; i--){
-                if(sc == ec){
+                if(sc == ec)
                     break;
-                }
                 arr[i][sc] = a++;
             }
             sr++;
