04faef0dc28e9e193890fdd7c5b6f0cd29e60c82
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-05-12 10:51:46 +0530

    Time: 27 ms (25.93%), Space: 112 MB (43.92%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -0,0 +1,27 @@
+class Solution {
+    long dp[];
+    public long slove(int ind, int[][] arr, long dp[]){
+        int n = arr.length;
+        if(ind >= n){
+            return 0;
+        }
+        if(dp[ind] != 0){
+            return dp[ind];
+        }
+        long notTake = 0;
+        
+        notTake = slove(ind + 1, arr, dp);
+        long take = 0;
+        
+        take = arr[ind][0] + slove(ind + arr[ind][1] + 1, arr, dp);
+        
+        return dp[ind] = Math.max(take,notTake);
+    }
+
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        dp = new long[n + 1];
+        
+        return slove(0, questions, dp); 
+    }
+}
