e595e2227712ba9f320c416e373c5efea4ca81f8
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-05-25 08:42:17 +0530

    Time: 4 ms (34.94%), Space: 43 MB (8.43%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.java
+++ 0837-new-21-game/0837-new-21-game.java
@@ -5,17 +5,14 @@ class Solution {
         double s = k > 0 ? 1 : 0;
         for (int i = 1; i <= n; i++) {
             dp[i] = s / maxPts;
-            if (i < k) {
+            if (i < k)
                 s += dp[i];
-            }
-            if (i - maxPts >= 0 && i - maxPts < k) {
+            if (i - maxPts >= 0 && i - maxPts < k)
                 s -= dp[i - maxPts];
-            }
         }
         double ans = 0;
-        for (int i = k; i <= n; i++) {
+        for (int i = k; i <= n; i++)
             ans += dp[i];
-        }
         return ans;
     }
 }
