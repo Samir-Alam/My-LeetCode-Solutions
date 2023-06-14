92a297e48de059b097a466ca75db236684814ada
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-06-14 07:00:16 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (26.93%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -13,24 +13,49 @@
  *     }
  * }
  */
+// class Solution {
+//     public int getMinimumDifference(TreeNode root) {
+//         List<Integer> l = new ArrayList<>();
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+//         while(!q.isEmpty()){
+//             TreeNode temp = q.poll();
+//             if(temp != null){
+//                 l.add(temp.val);
+//                 q.add(temp.left);
+//                 q.add(temp.right);
+//             }
+//         }
+//         Collections.sort(l);
+//         int diff = l.get(l.size()-1);
+//         for(int i = 1; i < l.size(); i++){
+//             diff = Math.min(diff, l.get(i) - l.get(i-1));
+//         }
+//         return diff;
+//     }
+// }
+
 class Solution {
-    public int getMinimumDifference(TreeNode root) {
-        List<Integer> l = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode temp = q.poll();
-            if(temp != null){
-                l.add(temp.val);
-                q.add(temp.left);
-                q.add(temp.right);
-            }
+    int minDifference = Integer.MAX_VALUE;
+    // Initially, it will be null.
+    TreeNode prevNode;
+
+    void inorderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
         }
-        Collections.sort(l);
-        int diff = l.get(l.size()-1);
-        for(int i = 1; i < l.size(); i++){
-            diff = Math.min(diff, l.get(i) - l.get(i-1));
+
+        inorderTraversal(node.left);
+        // Find the difference with the previous value if it is there.
+        if (prevNode != null) {
+            minDifference = Math.min(minDifference, node.val - prevNode.val);
         }
-        return diff;
+        prevNode = node;
+        inorderTraversal(node.right);
+    }
+
+    int getMinimumDifference(TreeNode root) {
+        inorderTraversal(root);
+        return minDifference;
     }
 }
