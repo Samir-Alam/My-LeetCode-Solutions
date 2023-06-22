a89292dd05b1baa1720963ea86f54af8480726b0
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-06-22 17:45:43 +0530

    Time: 4 ms (99.03%), Space: 54.4 MB (65.07%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,23 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-//         int n = prices.length;
-//         int free = 0, hold = -prices[0];
-        
-//         for (int i = 1; i < n; i++) {
-//             int tmp = hold;
-//             hold = Math.max(hold, free - prices[i]);
-//             free = Math.max(free, tmp + prices[i] - fee);
-//         }
-        
-//         return free;
-        
-        int n = prices.length;
-        int[] free = new int[n], hold = new int[n];
-        hold[0] = -prices[0];
-        for (int i = 1; i < n; i++) {
-            hold[i] = Math.max(hold[i - 1], free[i - 1] - prices[i]);
-            free[i] = Math.max(free[i - 1], hold[i - 1] + prices[i] - fee);
+        int buy = Integer.MIN_VALUE, sell = 0;
+        for (int price : prices) {
+            buy = Math.max(buy, sell - price);
+            sell = Math.max(sell, buy + price - fee);
         }
-        return free[n - 1];
+        return sell;
+        
+        // int n = prices.length;
+        // int[] free = new int[n], hold = new int[n];
+        // hold[0] = -prices[0];
+        // for (int i = 1; i < n; i++) {
+        //     hold[i] = Math.max(hold[i - 1], free[i - 1] - prices[i]);
+        //     free[i] = Math.max(free[i - 1], hold[i - 1] + prices[i] - fee);
+        // }
+        // return free[n - 1];
     }
 }
