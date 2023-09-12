c790c38759f7582ddd88d6239090454135404f35
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-09-12 18:19:20 +0530

    Time: 7 ms (98.85%), Space: 44.3 MB (78.89%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,18 +1,29 @@
 class Solution {
     public int minDeletions(String s) {
-        int[] lettersCount = new int[26];
-        for (char letter : s.toCharArray())
-            lettersCount[letter - 'a']++;
-        int numberOfDeletions = 0;
-        boolean[] usedFrequencies = new boolean[s.length() + 1];
-        for (int frequency : lettersCount) {
-            while (usedFrequencies[frequency]) {
-                frequency--;
-                numberOfDeletions++;
-            }
-            if (frequency > 0)
-                usedFrequencies[frequency] = true;
+        // int[] lettersCount = new int[26];
+        // for (char letter : s.toCharArray())
+        //     lettersCount[letter - 'a']++;
+        // int numberOfDeletions = 0;
+        // boolean[] usedFrequencies = new boolean[s.length() + 1];
+        // for (int frequency : lettersCount) {
+        //     while (usedFrequencies[frequency]) {
+        //         frequency--;
+        //         numberOfDeletions++;
+        //     }
+        //     if (frequency > 0)
+        //         usedFrequencies[frequency] = true;
+        // }
+        // return numberOfDeletions;
+        
+        int freq[] = new int[26];
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+        Arrays.sort(freq);
+        int keep = freq[25], prev = keep;
+        for (int i = 24; i >= 0 && freq[i] != 0 && prev != 0; i--) {
+            prev = Math.min(freq[i], prev - 1);
+            keep += prev;
         }
-        return numberOfDeletions;
+        return s.length() - keep;
     }
 }
