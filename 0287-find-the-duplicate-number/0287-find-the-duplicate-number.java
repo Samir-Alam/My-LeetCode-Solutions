e26402c2527638db8805877231272a8cbc96e807
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-09-19 14:45:47 +0530

    Time: 19 ms (34.25%), Space: 61.1 MB (5.10%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -18,9 +18,8 @@ class Solution {
         
         HashSet<Integer> seen = new HashSet<>();
         for (int num : nums) {
-            if (seen.contains(num)) {
+            if (seen.contains(num))
                 return num;
-            }
             seen.add(num);
         }
         return -1;
