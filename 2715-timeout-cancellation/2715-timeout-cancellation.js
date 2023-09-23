75f49317da0bda04e70a8e01bfd76cd68367c76f
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-09-23 18:28:16 +0530

    Time: 72 ms (12.14%), Space: 42.2 MB (21.27%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,43 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+   const timeOut = setTimeout(function() {
+       fn.apply(null, args);
+   }, t);
+    
+    const cancelFn = function() {
+        clearTimeout(timeOut);
+    };
+    
+    return cancelFn;
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const start = performance.now() 
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)})
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelT)
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
