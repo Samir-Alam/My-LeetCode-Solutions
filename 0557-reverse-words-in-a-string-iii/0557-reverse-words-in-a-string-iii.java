d602af2d9a4c40cdaee65ceedbe30b5620eead8c
Samir <66169191+Samir-Alam@users.noreply.github.com>
2023-10-01 21:04:39 +0530

    Time: 3 ms (98.87%), Space: 43.9 MB (92.70%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         String str[] = s.split(" ");
+//         String ans = "";
+//         for(String ss : str){
+//             ans += reverse(ss) + " ";
+//         }
+//         return ans.trim();
+//     }
+//     public String reverse(String s){
+//         StringBuilder sb = new StringBuilder();
+//         sb.append(s);
+//         sb.reverse();
+//         return sb.toString();
+//     }
+// }
 class Solution {
+
     public String reverseWords(String s) {
-        String str[] = s.split(" ");
-        String ans = "";
-        for(String ss : str){
-            ans += reverse(ss) + " ";
+        int lastSpaceIndex = -1;
+        char[] chArray = s.toCharArray();
+        int len = s.length();
+        for (int strIndex = 0; strIndex <= len; strIndex++) {
+            if (strIndex == len || chArray[strIndex] == ' ') {
+                int startIndex = lastSpaceIndex + 1;
+                int endIndex = strIndex - 1;
+                while (startIndex < endIndex) {
+                    char temp = chArray[startIndex];
+                    chArray[startIndex] = chArray[endIndex];
+                    chArray[endIndex] = temp;
+                    startIndex++;
+                    endIndex--;
+                }
+                lastSpaceIndex = strIndex;
+            }
         }
-        return ans.trim();
+        return new String(chArray);
     }
-    public String reverse(String s){
-        StringBuilder sb = new StringBuilder();
-        sb.append(s);
-        sb.reverse();
-        return sb.toString();
-    }
+
 }
