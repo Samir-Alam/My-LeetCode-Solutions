b8d0dc2a475f0c708535f29e2831995e797d7bde
Samir <66169191+Samir-Alam@users.noreply.github.com>
2024-02-05 22:16:08 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (51.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,37 +10,48 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // if(list1 == null)
+        //     return list2;
+        // else if(list2 == null)
+        //     return list1;
+        // ListNode head = new ListNode(), temp;
+        // if(list1.val <= list2.val){
+        //     head.val = list1.val;
+        //     list1 = list1.next;
+        // }else{
+        //     head.val = list2.val;
+        //     list2 = list2.next;
+        // }
+        // temp = head;
+        // while(list1 != null && list2 != null){
+        //     if(list1.val <= list2.val){
+        //         temp.next = new ListNode(list1.val);
+        //         temp = temp.next;
+        //         list1 = list1.next;
+        //     }else{
+        //         temp.next = new ListNode(list2.val);
+        //         temp = temp.next;
+        //         list2 = list2.next;
+        //     }
+        // }
+        // if(list1 != null)
+        //     temp.next = list1;
+        // else if(list2 != null)
+        //     temp.next = list2;
+        // return head;
+        
+        // Recursive Approach
         if(list1 == null)
             return list2;
         else if(list2 == null)
             return list1;
-        ListNode head = new ListNode(), temp;
         if(list1.val <= list2.val){
-            head.val = list1.val;
-            list1 = list1.next;
-        }else{
-            head.val = list2.val;
-            list2 = list2.next;
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }
-        temp = head;
-        while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
-                ListNode curr = new ListNode(list1.val);
-                temp.next = curr;
-                temp = temp.next;
-                list1 = list1.next;
-            }else{
-                ListNode curr = new ListNode(list2.val);
-                temp.next = curr;
-                temp = temp.next;
-                list2 = list2.next;
-            }
+        else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-        if(list1 != null){
-            temp.next = list1;
-        }
-        else if(list2 != null)
-            temp.next = list2;
-        return head;
     }
 }
