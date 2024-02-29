18cb29826aa29e188610db23533f1f2b25ce1a2a
Samir <66169191+Samir-Alam@users.noreply.github.com>
2024-02-29 17:57:04 +0530

    Time: 10 ms (87.30%), Space: 66.7 MB (22.46%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        if(root == null) return true;
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        boolean even = true;
+        while(q.size() > 0) {
+            int size = q.size();
+            int prevVal = even ? Integer.MIN_VALUE : Integer.MAX_VALUE; // init preVal based on level even or odd
+			while(size-- > 0) { // level by level
+                root = q.poll();
+                if(even && (root.val % 2 == 0 || root.val <= prevVal)) return false; // invalid case on even level
+                if(!even && (root.val % 2 == 1 || root.val >= prevVal)) return false; // invalid case on odd level
+                prevVal = root.val; // update the prev value
+                if(root.left != null) q.add(root.left); // add left child if exist
+                if(root.right != null) q.add(root.right); // add right child if exist
+            }
+            even = !even; // alter the levels
+        }
+        
+        return true;
+    }
+}
