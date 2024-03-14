fcdee01efbec5756d836b47ff67b33ae80122b75
Samir <66169191+Samir-Alam@users.noreply.github.com>
2024-03-14 23:27:44 +0530

    Time: 3 ms (69.27%), Space: 47.4 MB (73.36%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,15 +1,35 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int ans = 0;
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            int sum = 0;
-            for(int j = i; j < n; j++){
-                sum += nums[j];
-                if(sum == goal)
-                    ans++;
+        // int ans = 0;
+        // int n = nums.length;
+        // for(int i = 0; i < n; i++){
+        //     int sum = 0;
+        //     for(int j = i; j < n; j++){
+        //         sum += nums[j];
+        //         if(sum == goal)
+        //             ans++;
+        //     }
+        // }
+        // return ans;
+        
+        // Sliding Window
+        int start = 0;
+        int prefixZeros = 0;
+        int currentSum = 0;
+        int totalCount = 0;
+        for (int end = 0; end < nums.length; end++) {
+            currentSum += nums[end];
+            while (start < end && (nums[start] == 0 || currentSum > goal)) {
+                if (nums[start] == 1)
+                    prefixZeros = 0;
+                else
+                    prefixZeros++;
+                currentSum -= nums[start];
+                start++;
             }
+            if (currentSum == goal)
+                totalCount += 1 + prefixZeros;
         }
-        return ans;
+        return totalCount;
     }
 }
