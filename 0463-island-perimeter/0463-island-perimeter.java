d5c606e1b6a88fabb7e3fdcfd120f2845c096203
Samir <66169191+Samir-Alam@users.noreply.github.com>
2024-04-18 19:10:36 +0530

    Time: 4 ms (99.58%), Space: 45.7 MB (41.79%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -3,21 +3,17 @@ class Solution {
         int perimeter = 0;
         int rows = grid.length;
         int cols = grid[0].length;
-        
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (grid[r][c] == 1) {
                     perimeter += 4;
-                    if (r > 0 && grid[r-1][c] == 1) {
+                    if (r > 0 && grid[r-1][c] == 1)
                         perimeter -= 2;
-                    }
-                    if (c > 0 && grid[r][c-1] == 1) {
+                    if (c > 0 && grid[r][c-1] == 1)
                         perimeter -= 2;
-                    }
                 }
             }
         }
-        
         return perimeter;
     }
 }
