c10d146341d2d5f9dd2caf755ba4dcdf34ad8e81
Samir <66169191+Samir-Alam@users.noreply.github.com>
2024-12-14 14:49:06 +0530

    Time: 9 ms (50.73%), Space: 46.9 MB (9.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -3,19 +3,32 @@ class Solution {
         int n = arr.length;
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
         List<List<Integer>> ans = new ArrayList<>();
+        
+        // 1st
+        // for(int i = 0; i < n; i++){
+        //     int start = arr[i][0];
+        //     int end = arr[i][1];
+        //     if(!ans.isEmpty() && ans.get(ans.size()-1).get(1) >= end)
+        //         continue;
+        //     for(int j = i+1; j < n; j++){
+        //         if(arr[j][0] <= end)
+        //             end = Math.max(end, arr[j][1]);
+        //         else
+        //             break;
+        //     }
+        //     ans.add(Arrays.asList(start, end));
+        // }
+        
+        //2nd
         for(int i = 0; i < n; i++){
-            int start = arr[i][0];
-            int end = arr[i][1];
-            if(!ans.isEmpty() && ans.get(ans.size()-1).get(1) >= end)
-                continue;
-            for(int j = i+1; j < n; j++){
-                if(arr[j][0] <= end)
-                    end = Math.max(end, arr[j][1]);
-                else
-                    break;
+            if(ans.isEmpty() || arr[i][0] > ans.get(ans.size()-1).get(1))
+                ans.add(Arrays.asList(arr[i][0] , arr[i][1]));
+            else{
+                int temp = Math.max(ans.get(ans.size()-1).get(1), arr[i][1]);   
+                ans.get(ans.size()-1).set(1, temp);
             }
-            ans.add(Arrays.asList(start, end));
         }
+        
         int[][] res = new int[ans.size()][2];
         for(int i = 0; i < ans.size(); i++){
             res[i][0] = ans.get(i).get(0);
